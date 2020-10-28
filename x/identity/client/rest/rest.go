38c6aef4c64d0f435d29747e8e4ef213f2377a4c
ltacker <lucas.bertrand.22@gmail.com>
2020-10-28 11:36:47 +0100

    Implement REST routes

--- x/identity/client/rest/rest.go
+++ x/identity/client/rest/rest.go
@@ -1,26 +1,104 @@
 package rest
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	// this line is used by starport scaffolding # 1
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/tendermint/spn/x/identity/types"
 )
 
 const (
-    MethodGet = "GET"
+	MethodGet  = "GET"
+	MethodPost = "POST"
+)
+
+var (
+	UsernameRoute            = fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QueryUsername)
+	UsernameFromAddressRoute = fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QueryUsernameFromAddress)
+	SetUsernameRoute         = fmt.Sprintf("/%s/%s", types.QuerierRoute, "set-username")
 )
 
 // RegisterRoutes registers identity-related REST handlers to a router
 func RegisterRoutes(clientCtx client.Context, r *mux.Router) {
-	// this line is used by starport scaffolding # 2
+	registerQueryRoutes(clientCtx, r)
+	registerTxHandlers(clientCtx, r)
 }
 
 func registerQueryRoutes(clientCtx client.Context, r *mux.Router) {
-	// this line is used by starport scaffolding # 3
+	r.HandleFunc(UsernameRoute, usernameHandler(clientCtx)).Methods("GET")
+	r.HandleFunc(UsernameFromAddressRoute, usernameFromAddressHandler(clientCtx)).Methods("GET")
 }
 
 func registerTxHandlers(clientCtx client.Context, r *mux.Router) {
-	// this line is used by starport scaffolding # 4
+	r.HandleFunc(SetUsernameRoute, setUsernameHandler(clientCtx)).Methods("POST")
 }
 
+func usernameHandler(clientCtx client.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, height, err := clientCtx.QueryWithData(UsernameRoute, nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+
+		clientCtx = clientCtx.WithHeight(height)
+		rest.PostProcessResponse(w, clientCtx, res)
+	}
+}
+
+func usernameFromAddressHandler(clientCtx client.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, height, err := clientCtx.QueryWithData(UsernameFromAddressRoute, nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+
+		clientCtx = clientCtx.WithHeight(height)
+		rest.PostProcessResponse(w, clientCtx, res)
+	}
+}
+
+type setUsernameRequest struct {
+	BaseReq  rest.BaseReq `json:"base_req"`
+	Creator  string       `json:"creator"`
+	Username string       `json:"name"`
+}
+
+func setUsernameHandler(clientCtx client.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req setUsernameRequest
+		if !rest.ReadRESTReq(w, r, clientCtx.LegacyAmino, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		creator, err := sdk.AccAddressFromBech32(req.Creator)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		msg, err := types.NewMsgSetUsername(
+			creator,
+			req.Username,
+		)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		tx.WriteGeneratedTxResponse(clientCtx, w, req.BaseReq, msg)
+	}
+}
