77a052847e20b2e3f1ff1511395e0de59a7b2813
ltacker <lucas.bertrand.22@gmail.com>
2020-11-03 10:52:38 +0100

    Initialize chain type

---
+++ x/genesis/types/chain.go
@@ -0,0 +1,54 @@
+package types
+
+import (
+	"time"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// NewChain creates a new chain information object
+func NewChain(
+	chainID string,
+	creator string,
+	sourceURL string,
+	sourceHash string,
+	createdAt time.Time,
+	initialGenesis []byte,
+) (Chain, error) {
+	var chain Chain
+
+	if !checkChainID(chainID) {
+		chain, sdkerrors.Wrap(ErrInvalidChain, fmt.Sprintf("invalid chain ID %v", chainID))
+	}
+	chain.ChainID = chainID
+	chain.Creator = creator
+	chain.Peers = []string{}
+	chain.SourceURL = sourceURL
+	chain.SourceHash = sourceHash
+	chain.CreatedAt = createdAt.Unix()
+	chain.InitialGenesis = initialGenesis
+	chain.Final = false
+
+	return chain
+}
+
+// AppendPeer appends a new peer in the peer list of the chain
+func (c *Chain) AppendPeer(peer string) {
+	c.Peers = append(c.Peers, peer)
+}
+
+// checkChainID performs stateless verification of the chainID
+// The chainID must contain only alphanumeric character or hyphen 
+func checkChainID(chainID string) bool {
+	// Iterate characters
+	for _, c := range chainID {
+		if !isTagAuthorizedChar(c) {
+			return false
+		}
+	}
+	return true
+}
+
+// isTagAuthorizedChar checks if alphanumeric or hyphen character
+func isTagAuthorizedChar(c rune) bool {
+	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9') || c == '-'
+}
