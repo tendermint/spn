f044a4683024b6f0d93ba330b01cca3d8a647875
ltacker <lucas.bertrand.22@gmail.com>
2020-11-03 11:39:00 +0100

    Append votes in proposal state

--- x/genesis/types/errors.go
+++ x/genesis/types/errors.go
@@ -9,4 +9,5 @@ import (
 // x/genesis module sentinel errors
 var (
 	ErrInvalidChain = sdkerrors.Register(ModuleName, 1, "the chain is invalid")
+	ErrInvalidVote  = sdkerrors.Register(ModuleName, 2, "the vote is invalid")
 )
--- x/genesis/types/proposal.go
+++ x/genesis/types/proposal.go
@@ -1,7 +1,10 @@
 package types
 
 import (
+	"fmt"
 	"time"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // NewProposalInformation initializes a new proposal information structure
@@ -28,6 +31,45 @@ func NewProposalState() *ProposalState {
 	state.Status = ProposalState_PENDING
 
 	return &state
+}
+
+// ProposalAppendVote appends a new vote into the proposal
+func (ps *ProposalState) ProposalAppendVote(newVote *Vote) error {
+	// Check if the creator already voted
+	_, ok := ps.Votes[newVote.Creator]
+	if ok {
+		return sdkerrors.Wrap(ErrInvalidVote, fmt.Sprintf("the creator already voted"))
+	}
+
+	// Protobuf reset the map to nil if it has no value, therefore we must always check if it is initialized
+	if ps.Votes == nil {
+		ps.Votes = make(map[string]*Vote)
+	}
+	ps.Votes[newVote.Creator] = newVote
+
+	return nil
+}
+
+// NewVote creates a new proposal vote
+func NewVote(
+	voteID int32,
+	creator string,
+	createdAt time.Time,
+	value Vote_Value,
+) (*Vote, error) {
+	var vote Vote
+
+	// Check and set value
+	if value != Vote_APPROVE && value != Vote_REJECT {
+		return nil, sdkerrors.Wrap(ErrInvalidVote, fmt.Sprintf("vote must be approve or reject"))
+	}
+	vote.Value = value
+
+	vote.VoteID = voteID
+	vote.Creator = creator
+	vote.CreatedAt = createdAt.Unix()
+
+	return &vote, nil
 }
 
 // NewProposalChange creates a new proposal for a change in the genesis
