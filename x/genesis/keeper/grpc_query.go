0138d73a19b7665733cd2c1a7700d22359a317f2
ltacker <lucas.bertrand.22@gmail.com>
2020-11-04 16:12:06 +0100

    Squeleton for queries

--- x/genesis/keeper/grpc_query.go
+++ x/genesis/keeper/grpc_query.go
@@ -1,7 +1,55 @@
 package keeper
 
 import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/tendermint/spn/x/genesis/types"
 )
 
 var _ types.QueryServer = Keeper{}
+
+// ListChains lists the chains
+func (k Keeper) ListChains(
+	c context.Context,
+	req *types.QueryListChainsRequest,
+) (*types.QueryListChainsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	return nil, nil
+}
+
+// ShowChain describes a specific chain
+func (k Keeper) ShowChain(
+	c context.Context,
+	req *types.QueryShowChainRequest,
+) (*types.QueryShowChainResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	return nil, nil
+}
+
+// ListProposals lists the proposals for a chain
+func (k Keeper) ListProposals(
+	c context.Context,
+	req *types.QueryListProposalsRequest,
+) (*types.QueryListProposalsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	return nil, nil
+}
+
+// ShowProposal describes a specific proposal
+func (k Keeper) ShowProposal(
+	c context.Context,
+	req *types.QueryShowProposalRequest,
+) (*types.QueryShowProposalResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	return nil, nil
+}
