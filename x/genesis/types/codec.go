ff2b98f88b1995269ebcd079fe29ddf646d405f0
ltacker <lucas.bertrand.22@gmail.com>
2020-11-11 11:09:32 +0100

    Add new messages in codec

--- x/genesis/types/codec.go
+++ x/genesis/types/codec.go
@@ -3,15 +3,21 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
     cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	// this line is used by starport scaffolding # 1
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	// this line is used by starport scaffolding # 2
+	cdc.RegisterConcrete(MsgProposalAddAccount{}, "genesis/MsgProposalAddAccount", nil)
+	cdc.RegisterConcrete(MsgProposalAddValidator{}, "genesis/MsgProposalAddValidator", nil)
 } 
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	// this line is used by starport scaffolding # 3
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgProposalAddAccount{},
+		&MsgProposalAddValidator{},
+	)
 }
 
 var (
