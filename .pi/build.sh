a729a542f838df449499836b458ed3ca7fd5da0c
Jacob Gadikian <jacobgadikian@gmail.com>
2020-12-11 00:55:14 +0700

    Update build.sh
    fix build script

--- .pi/build.sh
+++ .pi/build.sh
@@ -43,13 +43,6 @@ rm ./.tmp/result-rootfs.tar
 # ===================================================================================
 
 
-# Unmount anything on the loop device
-sudo umount /dev/loop0p2 || true
-sudo umount /dev/loop0p1 || true
-
-# Detach from the loop device
-sudo losetup -d /dev/loop0 || true
-
 # Create a folder for images
 rm -rf images || true
 mkdir -p images
@@ -58,16 +51,16 @@ mkdir -p images
 fallocate -l 4G "images/spn.img"
 
 # loop-mount the image file so it becomes a disk
-sudo losetup --find --show images/sp.img
+export LOOP=$(sudo losetup --find --show images/spn.img)
 
 # partition the loop-mounted disk
-sudo parted --script /dev/loop0 mklabel msdos
-sudo parted --script /dev/loop0 mkpart primary fat32 0% 200M
-sudo parted --script /dev/loop0 mkpart primary ext4 200M 100%
+sudo parted --script $LOOP mklabel msdos
+sudo parted --script $LOOP mkpart primary fat32 0% 200M
+sudo parted --script $LOOP mkpart primary ext4 200M 100%
 
 # format the newly partitioned loop-mounted disk
-sudo mkfs.vfat -F32 /dev/loop0p1
-sudo mkfs.ext4 -F /dev/loop0p2
+sudo mkfs.vfat -F32 $(echo $LOOP)p1
+sudo mkfs.ext4 -F $(echo $LOOP)p2
 
 # Use the toolbox to copy the rootfs into the filesystem we formatted above.
 # * mount the disk's /boot and / partitions
@@ -76,12 +69,19 @@ sudo mkfs.ext4 -F /dev/loop0p2
 # soon will not use toolbox
 
 sudo mkdir -p mnt/boot mnt/rootfs
-sudo mount /dev/loop0p1 mnt/boot
-sudo mount /dev/loop0p2 mnt/rootfs
+sudo mount $(echo $LOOP)p1 mnt/boot
+sudo mount $(echo $LOOP)p2 mnt/rootfs
 sudo rsync -a ./.tmp/result-rootfs/boot/* mnt/boot
 sudo rsync -a ./.tmp/result-rootfs/* mnt/rootfs --exclude boot
 sudo mkdir mnt/rootfs/boot
 sudo umount mnt/boot mnt/rootfs
 
+# Tell pi where its memory card is:  This is needed only with the mainline linux kernel provied by linux-aarch64
+# sed -i 's/mmcblk0/mmcblk1/g' ./.tmp/result-rootfs/etc/fstab
+
 # Drop the loop mount
-sudo losetup -d /dev/loop0
+sudo losetup -d $LOOP
+
+# Delete .tmp and mnt
+sudo rm -rf ./.tmp
+sudo rm -rf mnt
