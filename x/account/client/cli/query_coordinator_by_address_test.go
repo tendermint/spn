76310199f0d459753c11eb4a532601c3fcdff86f
Pantani <danpantani@gmail.com>
2021-07-19 21:38:32 -0300

    remove ListCoordinatorByAddress commands and query

--- x/account/client/cli/query_coordinator_by_address_test.go
+++ x/account/client/cli/query_coordinator_by_address_test.go
@@ -5,9 +5,7 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	tmcli "github.com/tendermint/tendermint/libs/cli"
 	"google.golang.org/grpc/codes"
@@ -78,63 +76,4 @@ func TestShowCoordinatorByAddress(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestListCoordinatorByAddress(t *testing.T) {
-	net, objs := networkWithCoordinatorByAddressObjects(t, 5)
-
-	ctx := net.Validators[0].ClientCtx
-	request := func(next []byte, offset, limit uint64, total bool) []string {
-		args := []string{
-			fmt.Sprintf("--%s=json", tmcli.OutputFlag),
-		}
-		if next == nil {
-			args = append(args, fmt.Sprintf("--%s=%d", flags.FlagOffset, offset))
-		} else {
-			args = append(args, fmt.Sprintf("--%s=%s", flags.FlagPageKey, next))
-		}
-		args = append(args, fmt.Sprintf("--%s=%d", flags.FlagLimit, limit))
-		if total {
-			args = append(args, fmt.Sprintf("--%s", flags.FlagCountTotal))
-		}
-		return args
-	}
-	t.Run("ByOffset", func(t *testing.T) {
-		step := 2
-		for i := 0; i < len(objs); i += step {
-			args := request(nil, uint64(i), uint64(step), false)
-			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListCoordinatorByAddress(), args)
-			require.NoError(t, err)
-			var resp types.QueryAllCoordinatorByAddressResponse
-			require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-			for j := i; j < len(objs) && j < i+step; j++ {
-				assert.Equal(t, objs[j], resp.CoordinatorByAddress[j-i])
-			}
-		}
-	})
-	t.Run("ByKey", func(t *testing.T) {
-		step := 2
-		var next []byte
-		for i := 0; i < len(objs); i += step {
-			args := request(next, 0, uint64(step), false)
-			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListCoordinatorByAddress(), args)
-			require.NoError(t, err)
-			var resp types.QueryAllCoordinatorByAddressResponse
-			require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-			for j := i; j < len(objs) && j < i+step; j++ {
-				assert.Equal(t, objs[j], resp.CoordinatorByAddress[j-i])
-			}
-			next = resp.Pagination.NextKey
-		}
-	})
-	t.Run("Total", func(t *testing.T) {
-		args := request(nil, 0, uint64(len(objs)), true)
-		out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListCoordinatorByAddress(), args)
-		require.NoError(t, err)
-		var resp types.QueryAllCoordinatorByAddressResponse
-		require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-		require.NoError(t, err)
-		require.Equal(t, len(objs), int(resp.Pagination.Total))
-		require.Equal(t, objs, resp.CoordinatorByAddress)
-	})
 }
