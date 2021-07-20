d0b1e0dd563f99addbb3eea7f187eb38b71a7093
Pantani <danpantani@gmail.com>
2021-07-19 23:05:48 -0300

    add some test cases

--- x/account/keeper/msg_server_delete_coordinator_test.go
+++ x/account/keeper/msg_server_delete_coordinator_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/spn/x/account/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestMsgDeleteCoordinator(t *testing.T) {
@@ -13,7 +15,22 @@ func TestMsgDeleteCoordinator(t *testing.T) {
 		msg  types.MsgDeleteCoordinator
 		err  error
 	}{
-		// TODO: Add test cases.
+		{
+			name: "invalid address",
+			msg: types.MsgDeleteCoordinator{
+				Address: "invalid_address",
+			},
+			err: status.Error(codes.InvalidArgument,
+				"invalid coordinator address (invalid_address): decoding bech32 failed: invalid index of 1"),
+		}, {
+			name: "not found coordinator address",
+			msg: types.MsgDeleteCoordinator{
+				Address: "cosmos12330zcy9yez37lzrkm6d7fedcu7hc279sgkh3c",
+			},
+			err: status.Error(codes.NotFound,
+				"coordinator address not found: cosmos12330zcy9yez37lzrkm6d7fedcu7hc279sgkh3c"),
+		},
+		// TODO: Add more test cases.
 	}
 	srv, ctx := setupMsgServer(t)
 	for _, tt := range tests {
