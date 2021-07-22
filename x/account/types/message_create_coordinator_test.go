02d11b10673fea55becbf234dae3710c996de9d8
Pantani <danpantani@gmail.com>
2021-07-22 14:42:36 -0300

    add validation test

---
+++ x/account/types/message_create_coordinator_test.go
@@ -0,0 +1,36 @@
+package types
+
+import (
+	"testing"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMsgCreateCoordinator_ValidateBasic(t *testing.T) {
+	tests := []struct {
+		name string
+		msg  MsgCreateCoordinator
+		err  error
+	}{
+		{
+			name: "invalid address",
+			msg: MsgCreateCoordinator{
+				Address: "invalid address",
+			},
+			err: sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress,
+				"invalid address (invalid_address): decoding bech32 failed: invalid index of 1"),
+		},
+		// TODO add valid test cases
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
