8947eb9ef2aad70d87bf4b5f9d548bb6301a8cb8
Danilo Pantani <danpantani@gmail.com>
2021-07-22 16:15:05 -0300

    refactor(account): rename the module to avoid collision (#185)

---
+++ x/profile/keeper/grpc_query_coordinator_by_address.go
@@ -0,0 +1,27 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/tendermint/spn/x/profile/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) CoordinatorByAddress(c context.Context, req *types.QueryGetCoordinatorByAddressRequest) (*types.QueryGetCoordinatorByAddressResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(c)
+
+	val, found := k.GetCoordinatorByAddress(
+		ctx,
+		req.Address,
+	)
+	if !found {
+		return nil, status.Error(codes.InvalidArgument, "not found")
+	}
+
+	return &types.QueryGetCoordinatorByAddressResponse{CoordinatorByAddress: &val}, nil
+}
