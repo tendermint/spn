eb17df1b3a3db51e7ad4baee0c37ec4070cc16c7
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2021-07-23 18:31:53 +0200

    fix(launch): use interface for `InitialGenesis` of `Chain` (#193)
    * Add initial genesis interface
    
    * Add genesis types to codec
    
    * add tests
    
    * nullify byte array
    
    * test nil bytes
    
    * import

---
+++ testutil/sample/launch.go
@@ -0,0 +1,28 @@
+package sample
+
+import (
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	launch "github.com/tendermint/spn/x/launch/types"
+)
+
+func Chain(chainID string, coordinatorID uint64) *launch.Chain {
+	defaultGenesis, err := types.NewAnyWithValue((*launch.DefaultInitialGenesis)(nil))
+	if err != nil {
+		panic(err)
+	}
+
+	// Byte array is nullified in the store if empty
+	defaultGenesis.Value = []byte(nil)
+
+	return &launch.Chain{
+		ChainID:         chainID,
+		CoordinatorID:   coordinatorID,
+		CreatedAt:       time.Now().Unix(),
+		SourceURL:       String(10),
+		SourceHash:      String(10),
+		LaunchTriggered: false,
+		InitialGenesis:  defaultGenesis,
+	}
+}
