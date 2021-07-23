3379b396f9a0552c96a66c9899718943d0078821
Pantani <danpantani@gmail.com>
2021-07-23 15:45:50 -0300

    add more test cases

--- x/profile/keeper/msg_server_update_coordinator_address_test.go
+++ x/profile/keeper/msg_server_update_coordinator_address_test.go
@@ -12,10 +12,17 @@ import (
 
 func TestMsgUpdateCoordinatorAddress(t *testing.T) {
 	var (
-		addr1 = sample.AccAddress()
-		addr2 = sample.AccAddress()
-		//addr3 = sample.AccAddress()
+		addr   = sample.AccAddress()
+		coord1 = msgCreateCoordinator()
+		coord2 = msgCreateCoordinator()
 	)
+	srv, ctx := setupMsgServer(t)
+	if _, err := srv.CreateCoordinator(ctx, &coord1); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := srv.CreateCoordinator(ctx, &coord2); err != nil {
+		t.Fatal(err)
+	}
 	tests := []struct {
 		name string
 		msg  types.MsgUpdateCoordinatorAddress
@@ -25,21 +32,40 @@ func TestMsgUpdateCoordinatorAddress(t *testing.T) {
 		{
 			name: "not found address",
 			msg: types.MsgUpdateCoordinatorAddress{
-				Address:    addr1,
-				NewAddress: addr2,
+				Address:    addr,
+				NewAddress: addr,
 			},
-			err: sdkerrors.Wrap(types.ErrCoordAddressNotFound, addr1),
-			//}, {
-			//	name: "new address already exist",
-			//	msg: types.MsgUpdateCoordinatorAddress{
-			//		Address:    addr3,
-			//		NewAddress: addr2,
-			//	},
-			//err: sdkerrors.Wrap(types.ErrCoordAlreadyExist, "new address already have a coordinator: 1"),
+			err: sdkerrors.Wrap(types.ErrCoordAddressNotFound, addr),
+		}, {
+			name: "new address already exist",
+			msg: types.MsgUpdateCoordinatorAddress{
+				Address:    coord1.Address,
+				NewAddress: coord2.Address,
+			},
+			err: sdkerrors.Wrap(types.ErrCoordAlreadyExist, "new address already have a coordinator: 1"),
+		}, {
+			name: "update first coordinator address update",
+			msg: types.MsgUpdateCoordinatorAddress{
+				Address:    coord1.Address,
+				NewAddress: addr,
+			},
+			want: 0,
+		}, {
+			name: "update second coordinator address update",
+			msg: types.MsgUpdateCoordinatorAddress{
+				Address:    coord2.Address,
+				NewAddress: coord1.Address,
+			},
+			want: 1,
+		}, {
+			name: "new address already updated",
+			msg: types.MsgUpdateCoordinatorAddress{
+				Address:    addr,
+				NewAddress: coord1.Address,
+			},
+			err: sdkerrors.Wrap(types.ErrCoordAlreadyExist, "new address already have a coordinator: 0"),
 		},
-		// TODO: valid tests cases
 	}
-	srv, ctx := setupMsgServer(t)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := srv.UpdateCoordinatorAddress(ctx, &tt.msg)
