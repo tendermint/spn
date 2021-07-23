554a820ca9dad06253ce2e37c4487212ff61af05
Pantani <danpantani@gmail.com>
2021-07-23 16:37:08 -0300

    add generated js

--- vue/src/store/generated/tendermint/spn/tendermint.spn.profile/index.js
+++ vue/src/store/generated/tendermint/spn/tendermint.spn.profile/index.js
@@ -163,23 +163,6 @@ export default {
                 throw new SpVuexError('QueryClient:QueryCoordinatorAll', 'API Node Unavailable. Could not perform query: ' + e.message);
             }
         },
-        async sendMsgUpdateCoordinatorAddress({ rootGetters }, { value, fee = [], memo = '' }) {
-            try {
-                const txClient = await initTxClient(rootGetters);
-                const msg = await txClient.msgUpdateCoordinatorAddress(value);
-                const result = await txClient.signAndBroadcast([msg], { fee: { amount: fee,
-                        gas: "200000" }, memo });
-                return result;
-            }
-            catch (e) {
-                if (e == MissingWalletError) {
-                    throw new SpVuexError('TxClient:MsgUpdateCoordinatorAddress:Init', 'Could not initialize signing client. Wallet is required.');
-                }
-                else {
-                    throw new SpVuexError('TxClient:MsgUpdateCoordinatorAddress:Send', 'Could not broadcast Tx: ' + e.message);
-                }
-            }
-        },
         async sendMsgCreateCoordinator({ rootGetters }, { value, fee = [], memo = '' }) {
             try {
                 const txClient = await initTxClient(rootGetters);
@@ -197,18 +180,20 @@ export default {
                 }
             }
         },
-        async MsgUpdateCoordinatorAddress({ rootGetters }, { value }) {
+        async sendMsgUpdateCoordinatorAddress({ rootGetters }, { value, fee = [], memo = '' }) {
             try {
                 const txClient = await initTxClient(rootGetters);
                 const msg = await txClient.msgUpdateCoordinatorAddress(value);
-                return msg;
+                const result = await txClient.signAndBroadcast([msg], { fee: { amount: fee,
+                        gas: "200000" }, memo });
+                return result;
             }
             catch (e) {
                 if (e == MissingWalletError) {
                     throw new SpVuexError('TxClient:MsgUpdateCoordinatorAddress:Init', 'Could not initialize signing client. Wallet is required.');
                 }
                 else {
-                    throw new SpVuexError('TxClient:MsgUpdateCoordinatorAddress:Create', 'Could not create message: ' + e.message);
+                    throw new SpVuexError('TxClient:MsgUpdateCoordinatorAddress:Send', 'Could not broadcast Tx: ' + e.message);
                 }
             }
         },
@@ -224,6 +209,21 @@ export default {
                 }
                 else {
                     throw new SpVuexError('TxClient:MsgCreateCoordinator:Create', 'Could not create message: ' + e.message);
+                }
+            }
+        },
+        async MsgUpdateCoordinatorAddress({ rootGetters }, { value }) {
+            try {
+                const txClient = await initTxClient(rootGetters);
+                const msg = await txClient.msgUpdateCoordinatorAddress(value);
+                return msg;
+            }
+            catch (e) {
+                if (e == MissingWalletError) {
+                    throw new SpVuexError('TxClient:MsgUpdateCoordinatorAddress:Init', 'Could not initialize signing client. Wallet is required.');
+                }
+                else {
+                    throw new SpVuexError('TxClient:MsgUpdateCoordinatorAddress:Create', 'Could not create message: ' + e.message);
                 }
             }
         },
--- vue/src/store/generated/tendermint/spn/tendermint.spn.profile/module/index.ts
+++ vue/src/store/generated/tendermint/spn/tendermint.spn.profile/module/index.ts
@@ -4,13 +4,13 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgUpdateCoordinatorAddress } from "./types/profile/tx";
 import { MsgCreateCoordinator } from "./types/profile/tx";
+import { MsgUpdateCoordinatorAddress } from "./types/profile/tx";
 
 
 const types = [
-  ["/tendermint.spn.profile.MsgUpdateCoordinatorAddress", MsgUpdateCoordinatorAddress],
   ["/tendermint.spn.profile.MsgCreateCoordinator", MsgCreateCoordinator],
+  ["/tendermint.spn.profile.MsgUpdateCoordinatorAddress", MsgUpdateCoordinatorAddress],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -39,8 +39,8 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
-    msgUpdateCoordinatorAddress: (data: MsgUpdateCoordinatorAddress): EncodeObject => ({ typeUrl: "/tendermint.spn.profile.MsgUpdateCoordinatorAddress", value: data }),
     msgCreateCoordinator: (data: MsgCreateCoordinator): EncodeObject => ({ typeUrl: "/tendermint.spn.profile.MsgCreateCoordinator", value: data }),
+    msgUpdateCoordinatorAddress: (data: MsgUpdateCoordinatorAddress): EncodeObject => ({ typeUrl: "/tendermint.spn.profile.MsgUpdateCoordinatorAddress", value: data }),
     
   };
 };
