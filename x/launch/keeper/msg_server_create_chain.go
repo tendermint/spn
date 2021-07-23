71746c0b5b48ba0ac9e40e9c0fa6c1738e99cab9
ltacker <lucas.bertrand.22@gmail.com>
2021-07-23 17:51:37 +0200

    Implement message

--- x/launch/keeper/msg_server_create_chain.go
+++ x/launch/keeper/msg_server_create_chain.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	"fmt"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tendermint/spn/x/launch/types"
@@ -11,9 +13,45 @@ func (k msgServer) CreateChain(goCtx context.Context, msg *types.MsgCreateChain)
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// Get the coordinator ID associated to the sender address
-	_ = ctx
+	coordId, found := k.profileKeeper.CoordinatorIdFromAddress(ctx, msg.Coordinator)
+	if !found {
+		return nil, sdkerrors.Wrap(types.ErrCoordinatorNotExist, msg.Coordinator)
+	}
 
-	//
+	// Compute the chain id
+	chainNameCount, found := k.GetChainNameCount(ctx, msg.ChainName)
+	if !found {
+		chainNameCount = types.ChainNameCount{
+			msg.ChainName,
+			0,
+		}
+	}
+	chainID := types.ChainIdFromChainName(msg.ChainName, chainNameCount.Count)
+	chainNameCount.Count++
 
-	return &types.MsgCreateChainResponse{}, nil
+	// chainID must always be unique by design
+	// if it already exists then something is wrong in the protocol
+	_, found = k.GetChain(ctx, chainID)
+	if found {
+		panic(fmt.Sprintf("chain id %s already exists", chainID))
+	}
+
+	// Initialize the chain
+	chain := types.Chain{
+		ChainID: chainID,
+		CoordinatorID: coordId,
+		CreatedAt: ctx.BlockTime().Unix(),
+		SourceURL: msg.SourceURL,
+		SourceHash: msg.SourceHash,
+		LaunchTriggered: false,
+		LaunchTimestamp: 0,
+	}
+
+	// Store values
+	k.SetChain(ctx, chain)
+	k.SetChainNameCount(ctx, chainNameCount)
+
+	return &types.MsgCreateChainResponse{
+		ChainID: chainID,
+	}, nil
 }
---
+++ x/launch/types/chain.go
@@ -0,0 +1,35 @@
+package types
+
+import (
+	"errors"
+	"fmt"
+)
+
+// ChainIdFromChainName returns the chain id from the chain name and the count
+func ChainIdFromChainName(chainName string, chainNameCount uint64) string {
+	return fmt.Sprintf("%v-%v", chainName, chainNameCount)
+}
+
+// CheckChainName verifies the name is valid as a chain name
+func CheckChainName(chainName string) error {
+	// TODO: Determine final check: https://github.com/tendermint/spn/issues/194
+
+	// No empty
+	if len(chainName) == 0 {
+		return errors.New("chain name can't be empty")
+	}
+
+	// Iterate characters
+	for _, c := range chainName {
+		if !isChainAuthorizedChar(c) {
+			return errors.New("chain name must be alphanumerical")
+		}
+	}
+
+	return nil
+}
+
+// isChainAuthorizedChar checks to ensure that all letters in the chain name are valid
+func isChainAuthorizedChar(c rune) bool {
+	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9')
+}
