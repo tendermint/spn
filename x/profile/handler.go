944f34133ddf431402accf4932f925db2eee92b3
Danilo Pantani <danpantani@gmail.com>
2021-07-27 06:31:02 -0300

    feat(profile): allows to update a coordinator address (#186)

--- x/profile/handler.go
+++ x/profile/handler.go
@@ -22,6 +22,10 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 			res, err := msgServer.CreateCoordinator(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 
+		case *types.MsgUpdateCoordinatorAddress:
+			res, err := msgServer.UpdateCoordinatorAddress(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
---
+++ x/profile/types/message_update_coordinator_address_test.go
@@ -0,0 +1,59 @@
+package types
+
+import (
+	"testing"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/spn/testutil/sample"
+)
+
+func TestMsgUpdateCoordinatorAddress_ValidateBasic(t *testing.T) {
+	addr := sample.AccAddress()
+	tests := []struct {
+		name string
+		msg  MsgUpdateCoordinatorAddress
+		err  error
+	}{
+		{
+			name: "invalid address",
+			msg: MsgUpdateCoordinatorAddress{
+				Address:    "invalid address",
+				NewAddress: sample.AccAddress(),
+			},
+			err: sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress,
+				"invalid address (invalid_address): decoding bech32 failed: invalid index of 1"),
+		}, {
+			name: "invalid new address",
+			msg: MsgUpdateCoordinatorAddress{
+				Address:    sample.AccAddress(),
+				NewAddress: "invalid address",
+			},
+			err: sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress,
+				"invalid new address (invalid_address): decoding bech32 failed: invalid index of 1"),
+		}, {
+			name: "equal addresses",
+			msg: MsgUpdateCoordinatorAddress{
+				Address:    addr,
+				NewAddress: addr,
+			},
+			err: sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "address are equal of new address (%s)", addr),
+		}, {
+			name: "valid addresses",
+			msg: MsgUpdateCoordinatorAddress{
+				Address:    sample.AccAddress(),
+				NewAddress: sample.AccAddress(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.msg.ValidateBasic()
+			if tt.err != nil {
+				require.ErrorIs(t, err, tt.err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
