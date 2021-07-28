5fa0497370bb3a23962874343f85b5f654d4d051
Pantani <danpantani@gmail.com>
2021-07-28 12:57:29 -0300

    uses require instead assert for test

--- x/profile/types/genesis_test.go
+++ x/profile/types/genesis_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/spn/testutil/sample"
 )
@@ -47,7 +46,7 @@ func TestGenesisStateValidateValidator(t *testing.T) {
 				err := tt.genState.validateValidators()
 				if tt.err != nil {
 					require.Error(t, err)
-					assert.Equal(t, tt.err.Error(), err.Error())
+					require.Equal(t, tt.err.Error(), err.Error())
 					return
 				}
 				require.NoError(t, err)
@@ -160,7 +159,7 @@ func TestGenesisStateValidateCoordinator(t *testing.T) {
 				err := tt.genState.validateCoordinators()
 				if tt.err != nil {
 					require.Error(t, err)
-					assert.Equal(t, tt.err.Error(), err.Error())
+					require.Equal(t, tt.err.Error(), err.Error())
 					return
 				}
 				require.NoError(t, err)
