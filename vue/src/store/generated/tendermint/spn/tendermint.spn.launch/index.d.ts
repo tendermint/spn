71ca764dec64283b47c26c83ab3cda4451fbd299
ltacker <lucas.bertrand.22@gmail.com>
2021-07-28 10:20:31 +0200

    Remove vue files

--- vue/src/store/generated/tendermint/spn/tendermint.spn.launch/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ChainNameCount } from "./module/types/launch/chain";
-import { Chain } from "./module/types/launch/chain";
-import { DefaultInitialGenesis } from "./module/types/launch/chain";
-import { GenesisURL } from "./module/types/launch/chain";
-import { GenesisAccount } from "./module/types/launch/genesis_account";
-export { ChainNameCount, Chain, DefaultInitialGenesis, GenesisURL, GenesisAccount };
-declare const _default;
-export default _default;
