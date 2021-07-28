e87863119cb7d8361a1e53435bb37c13a5be4e24
Pantani <danpantani@gmail.com>
2021-07-27 22:19:44 -0300

    assert data inside keeper for unit tests

--- x/profile/keeper/msg_server_create_coordinator_test.go
+++ x/profile/keeper/msg_server_create_coordinator_test.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -24,39 +25,48 @@ func msgCreateCoordinator() types.MsgCreateCoordinator {
 
 func TestMsgCreateCoordinator(t *testing.T) {
 	var (
-		msg1 = msgCreateCoordinator()
-		msg2 = msgCreateCoordinator()
+		msg1        = msgCreateCoordinator()
+		msg2        = msgCreateCoordinator()
+		ctx, k, srv = setupMsgServer(t)
+		wCtx        = sdk.WrapSDKContext(ctx)
 	)
 	tests := []struct {
-		name string
-		msg  types.MsgCreateCoordinator
-		want uint64
-		err  error
+		name   string
+		msg    types.MsgCreateCoordinator
+		wantId uint64
+		err    error
 	}{
 		{
-			name: "valid coordinator 1",
-			msg:  msg1,
-			want: 0,
+			name:   "valid coordinator 1",
+			msg:    msg1,
+			wantId: 0,
 		}, {
-			name: "valid coordinator 2",
-			msg:  msg2,
-			want: 1,
+			name:   "valid coordinator 2",
+			msg:    msg2,
+			wantId: 1,
 		}, {
 			name: "already exist address",
 			msg:  msg2,
 			err:  sdkerrors.Wrap(types.ErrCoordAlreadyExist, "coordinatorId: 1"),
 		},
 	}
-	srv, ctx := setupMsgServer(t)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := srv.CreateCoordinator(ctx, &tt.msg)
+			_, err := srv.CreateCoordinator(wCtx, &tt.msg)
 			if tt.err != nil {
 				require.ErrorIs(t, err, tt.err)
 				return
 			}
 			require.NoError(t, err)
-			assert.EqualValues(t, tt.want, got.CoordinatorId)
+			coordByAddr, found := k.GetCoordinatorByAddress(ctx, tt.msg.Address)
+			assert.True(t, found, "coordinator by address not found")
+			assert.EqualValues(t, tt.wantId, coordByAddr.CoordinatorId)
+
+			coord := k.GetCoordinator(ctx, coordByAddr.CoordinatorId)
+			assert.True(t, found, "coordinator id not found")
+			assert.EqualValues(t, tt.msg.Address, coord.Address)
+			assert.EqualValues(t, tt.msg.Description, coord.Description)
+			assert.EqualValues(t, coordByAddr.CoordinatorId, coord.CoordinatorId)
 		})
 	}
 }
--- x/profile/keeper/msg_server_update_coordinator_address_test.go
+++ x/profile/keeper/msg_server_update_coordinator_address_test.go
@@ -3,7 +3,9 @@ package keeper
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/spn/testutil/sample"
 	"github.com/tendermint/spn/x/profile/types"
@@ -11,21 +13,21 @@ import (
 
 func TestMsgUpdateCoordinatorAddress(t *testing.T) {
 	var (
-		addr   = sample.AccAddress()
-		coord1 = msgCreateCoordinator()
-		coord2 = msgCreateCoordinator()
+		addr        = sample.AccAddress()
+		coord1      = msgCreateCoordinator()
+		coord2      = msgCreateCoordinator()
+		ctx, k, srv = setupMsgServer(t)
+		wCtx        = sdk.WrapSDKContext(ctx)
 	)
-	srv, ctx := setupMsgServer(t)
-	if _, err := srv.CreateCoordinator(ctx, &coord1); err != nil {
+	if _, err := srv.CreateCoordinator(wCtx, &coord1); err != nil {
 		t.Fatal(err)
 	}
-	if _, err := srv.CreateCoordinator(ctx, &coord2); err != nil {
+	if _, err := srv.CreateCoordinator(wCtx, &coord2); err != nil {
 		t.Fatal(err)
 	}
 	tests := []struct {
 		name string
 		msg  types.MsgUpdateCoordinatorAddress
-		want uint64
 		err  error
 	}{
 		{
@@ -48,14 +50,12 @@ func TestMsgUpdateCoordinatorAddress(t *testing.T) {
 				Address:    coord1.Address,
 				NewAddress: addr,
 			},
-			want: 0,
 		}, {
 			name: "update second coordinator address update",
 			msg: types.MsgUpdateCoordinatorAddress{
 				Address:    coord2.Address,
 				NewAddress: coord1.Address,
 			},
-			want: 1,
 		}, {
 			name: "new address already updated",
 			msg: types.MsgUpdateCoordinatorAddress{
@@ -67,12 +67,24 @@ func TestMsgUpdateCoordinatorAddress(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, err := srv.UpdateCoordinatorAddress(ctx, &tt.msg)
+			_, err := srv.UpdateCoordinatorAddress(wCtx, &tt.msg)
 			if tt.err != nil {
 				require.ErrorIs(t, err, tt.err)
 				return
 			}
 			require.NoError(t, err)
+
+			_, found := k.GetCoordinatorByAddress(ctx, tt.msg.Address)
+			assert.False(t, found, "old coordinator address was not removed")
+
+			coordByAddr, found := k.GetCoordinatorByAddress(ctx, tt.msg.NewAddress)
+			assert.True(t, found, "coordinator by address not found")
+			assert.EqualValues(t, tt.msg.NewAddress, coordByAddr.Address)
+
+			coord := k.GetCoordinator(ctx, coordByAddr.CoordinatorId)
+			assert.True(t, found, "coordinator id not found")
+			assert.EqualValues(t, tt.msg.NewAddress, coord.Address)
+			assert.EqualValues(t, coordByAddr.CoordinatorId, coord.CoordinatorId)
 		})
 	}
 }
