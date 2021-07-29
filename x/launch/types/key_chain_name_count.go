9067403b33de31ea08476f8f3839d004c78b638b
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2021-07-29 18:00:17 +0200

    Update x/launch/types/key_chain_name_count.go
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>

--- x/launch/types/key_chain_name_count.go
+++ x/launch/types/key_chain_name_count.go
@@ -11,11 +11,5 @@ const (
 
 // ChainNameCountKey returns the store key to retrieve a ChainNameCount from the index fields
 func ChainNameCountKey(chainName string) []byte {
-	var key []byte
-
-	chainNameBytes := []byte(chainName)
-	key = append(key, chainNameBytes...)
-	key = append(key, []byte("/")...)
-
-	return key
+	return []byte(chainName+"/")}
 }
