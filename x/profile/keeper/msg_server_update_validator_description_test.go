36f05839511dcdae5b29b2f77d09dfdc469aca3d
Pantani <danpantani@gmail.com>
2021-07-29 15:00:38 -0300

    assert keeper data for update validator description tests

--- x/profile/keeper/msg_server_update_validator_description_test.go
+++ x/profile/keeper/msg_server_update_validator_description_test.go
@@ -24,7 +24,7 @@ func TestMsgUpdateValidatorDescription(t *testing.T) {
 	var (
 		addr1       = sample.AccAddress()
 		addr2       = sample.AccAddress()
-		ctx, k, srv = setupMsgServerAndKeeper(t)
+		ctx, k, srv = setupMsgServer(t)
 		wCtx        = sdk.WrapSDKContext(ctx)
 	)
 	tests := []struct {
@@ -54,30 +54,47 @@ func TestMsgUpdateValidatorDescription(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			oldValidator, oldFound := k.GetValidator(ctx, tt.msg.Address)
+
 			_, err := srv.UpdateValidatorDescription(wCtx, &tt.msg)
 			if tt.err != nil {
 				require.ErrorIs(t, err, tt.err)
 				return
 			}
 			require.NoError(t, err)
+
 			validator, found := k.GetValidator(ctx, tt.msg.Address)
 			assert.True(t, found, "validator not found")
 			assert.EqualValues(t, tt.msg.Address, validator.Address)
 
 			if len(tt.msg.Description.Identity) > 0 {
 				assert.EqualValues(t, tt.msg.Description.Identity, validator.Description.Identity)
+			} else if oldFound {
+				require.EqualValues(t, oldValidator.Description.Identity, oldValidator.Description.Identity)
 			}
+
 			if len(tt.msg.Description.Website) > 0 {
 				assert.EqualValues(t, tt.msg.Description.Website, validator.Description.Website)
+			} else if oldFound {
+				require.EqualValues(t, oldValidator.Description.Website, oldValidator.Description.Website)
 			}
+
 			if len(tt.msg.Description.Details) > 0 {
 				assert.EqualValues(t, tt.msg.Description.Details, validator.Description.Details)
+			} else if oldFound {
+				require.EqualValues(t, oldValidator.Description.Details, oldValidator.Description.Details)
 			}
+
 			if len(tt.msg.Description.Moniker) > 0 {
 				assert.EqualValues(t, tt.msg.Description.Moniker, validator.Description.Moniker)
+			} else if oldFound {
+				require.EqualValues(t, oldValidator.Description.Moniker, oldValidator.Description.Moniker)
 			}
+
 			if len(tt.msg.Description.SecurityContact) > 0 {
 				assert.EqualValues(t, tt.msg.Description.SecurityContact, validator.Description.SecurityContact)
+			} else if oldFound {
+				require.EqualValues(t, oldValidator.Description.SecurityContact, oldValidator.Description.SecurityContact)
 			}
 		})
 	}
