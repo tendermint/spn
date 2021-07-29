137c1e64f766bf5fbef7fc92de25d5bfa535f295
ltacker <lucas.bertrand.22@gmail.com>
2021-07-29 14:11:55 +0200

    Fix delete coordinator test

--- x/profile/keeper/msg_server_delete_coordinator_test.go
+++ x/profile/keeper/msg_server_delete_coordinator_test.go
@@ -14,7 +14,7 @@ import (
 func TestMsgDeleteCoordinator(t *testing.T) {
 	var (
 		addr        = sample.AccAddress()
-		msgCoord    = msgCreateCoordinator()
+		msgCoord    = sample.MsgCreateCoordinator(sample.AccAddress())
 		ctx, k, srv = setupMsgServerAndKeeper(t)
 		wCtx        = sdk.WrapSDKContext(ctx)
 	)
