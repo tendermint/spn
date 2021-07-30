3b0e3e5ab69c7adc6756d4a06ac41f974d2558d0
ltacker <lucas.bertrand.22@gmail.com>
2021-07-30 11:27:10 +0200

    Flags

--- x/launch/client/cli/tx_edit_chain.go
+++ x/launch/client/cli/tx_edit_chain.go
@@ -8,6 +8,10 @@ import (
 	"github.com/tendermint/spn/x/launch/types"
 )
 
+const (
+	flagEditInitialGenesis  = "edit-initial-genesis"
+)
+
 func CmdEditChain() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "edit-chain [chainID] [sourceURL] [sourceHash]",
@@ -15,6 +19,17 @@ func CmdEditChain() *cobra.Command {
 		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			genesisURL, err := cmd.Flags().GetString(flagGenesisURL)
+			if err != nil {
+				return err
+			}
+
+			// TODO: automatically determine this value by fetching the resource (need to determine the hash before)
+			genesisHash, err := cmd.Flags().GetString(flagGenesisHash)
 			if err != nil {
 				return err
 			}
@@ -32,6 +47,8 @@ func CmdEditChain() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().String(flagGenesisURL, "", "URL for a custom genesis")
+	cmd.Flags().String(flagGenesisHash, "", "hash of the content of the custom genesis")
 	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
