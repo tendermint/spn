b9ecbb4a411497b8c1e5d0a7a95ac5d1873d0e91
ltacker <lucas.bertrand.22@gmail.com>
2021-08-04 15:40:40 +0200

    essage logic

--- x/launch/keeper/msg_server_revert_launch.go
+++ x/launch/keeper/msg_server_revert_launch.go
@@ -2,7 +2,11 @@ package keeper
 
 import (
 	"context"
+	"fmt"
+	spnerrors "github.com/tendermint/spn/pkg/errors"
 
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	profiletypes "github.com/tendermint/spn/x/profile/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tendermint/spn/x/launch/types"
 )
@@ -10,8 +14,36 @@ import (
 func (k msgServer) RevertLaunch(goCtx context.Context, msg *types.MsgRevertLaunch) (*types.MsgRevertLaunchResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	chain, found := k.GetChain(ctx, msg.ChainID)
+	if !found {
+		return nil, sdkerrors.Wrap(types.ErrChainNotFound, msg.ChainID)
+	}
+
+	// Check sender is the coordinator of the chain
+	coordinatorID, found := k.profileKeeper.CoordinatorIDFromAddress(ctx, msg.Coordinator)
+	if !found {
+		return nil, sdkerrors.Wrap(profiletypes.ErrCoordAddressNotFound, msg.Coordinator)
+	}
+	if chain.CoordinatorID != coordinatorID {
+		return nil, sdkerrors.Wrap(profiletypes.ErrCoordInvalid, fmt.Sprintf(
+			"coordinator of the chain is %v",
+			chain.CoordinatorID,
+		))
+	}
+
+	if !chain.LaunchTriggered {
+		return nil, sdkerrors.Wrap(types.ErrLaunchNotTriggered, msg.ChainID)
+	}
+
+	// The LaunchTimestamp must always be a non-zero value if LaunchTriggered is set
+	if chain.LaunchTimestamp == 0 {
+		return nil, spnerrors.Critical("LaunchTimestamp is not set while LaunchTriggered is set")
+	}
+
+	// We must wait for a specific delay once the chain is launched before being able to revert it
+	if ctx.BlockTime().Unix() < chain.LaunchTimestamp + types.REVERT_DELAY {
+		return nil, sdkerrors.Wrap(types.ErrRevertDelayNotReached, msg.ChainID)
+	}
 
 	return &types.MsgRevertLaunchResponse{}, nil
 }
--- x/launch/types/errors.go
+++ x/launch/types/errors.go
@@ -8,5 +8,9 @@ import (
 
 // x/launch module sentinel errors
 var (
-	ErrInvalidChainName = sdkerrors.Register(ModuleName, 1, "the chain name is invalid")
+	ErrChainNotFound     = sdkerrors.Register(ModuleName, 1, "chain not found")
+	ErrInvalidChainName = sdkerrors.Register(ModuleName, 2, "the chain name is invalid")
+	ErrLaunchNotTriggered   = sdkerrors.Register(ModuleName, 13, "the chain launch has not been triggered")
+	ErrRevertDelayNotReached   = sdkerrors.Register(ModuleName, 14, "the revert delay has not been reached")
+
 )
