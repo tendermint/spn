11d2887fc769c8945165175c86e24f756dc249ff
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2021-08-06 17:32:15 +0200

    feat: add `Validate` method to `InitialGenesis` (#229)

--- x/launch/types/initial_genesis.go
+++ x/launch/types/initial_genesis.go
@@ -1,4 +1,28 @@
 package types
 
+import "errors"
+
 // InitialGenesis defines the interface for initial genesis types
-type InitialGenesis interface{}
+type InitialGenesis interface {
+	Validate() error
+}
+
+var _ InitialGenesis = &DefaultInitialGenesis{}
+
+// Validate implements InitialGenesis
+func (DefaultInitialGenesis) Validate() error { return nil }
+
+var _ InitialGenesis = &GenesisURL{}
+
+const HashLength = 64
+
+// Validate implements InitialGenesis
+func (g GenesisURL) Validate() error {
+	if g.Url == "" {
+		return errors.New("no url provided")
+	}
+	if len(g.Hash) != HashLength {
+		return errors.New("hash must be sha256")
+	}
+	return nil
+}
