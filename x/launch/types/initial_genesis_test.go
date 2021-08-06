1f0009a84c61f3ce1f0d76e49a5b98148d025e53
ltacker <lucas.bertrand.22@gmail.com>
2021-08-06 11:47:03 +0200

    Add validate function

---
+++ x/launch/types/initial_genesis_test.go
@@ -0,0 +1,37 @@
+package types_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/spn/testutil/sample"
+	"github.com/tendermint/spn/x/launch/types"
+	"testing"
+)
+
+func TestDefaultInitialGenesis_Validate(t *testing.T) {
+	require.NoError(t, types.DefaultInitialGenesis{}.Validate(),
+	" DefaultInitialGenesis should always be valid",
+	)
+}
+
+func TestGenesisURL_Validate(t *testing.T) {
+	require.NoError(t, types.GenesisURL{
+		Url: sample.String(30),
+		Hash: sample.GenesisHash(),
+	}.Validate(),
+		" GenesisURL should be valid",
+	)
+
+	require.Error(t, types.GenesisURL{
+		Url: "",
+		Hash: sample.GenesisHash(),
+	}.Validate(),
+		" GenesisURL must contain a url",
+	)
+
+	require.Error(t, types.GenesisURL{
+		Url: sample.String(30),
+		Hash: sample.String(types.HashLength-1),
+	}.Validate(),
+		" GenesisURL must contain a valid sha256 hash",
+	)
+}
