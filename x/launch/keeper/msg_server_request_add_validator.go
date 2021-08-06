ff20aa9a0ca62f7507027f9aab29b3970e34982e
ltacker <lucas.bertrand.22@gmail.com>
2021-08-06 14:43:10 +0200

    Implement the message

--- x/launch/keeper/msg_server_request_add_validator.go
+++ x/launch/keeper/msg_server_request_add_validator.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	codec "github.com/cosmos/cosmos-sdk/codec/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tendermint/spn/x/launch/types"
@@ -10,8 +12,35 @@ import (
 func (k msgServer) RequestAddValidator(goCtx context.Context, msg *types.MsgRequestAddValidator) (*types.MsgRequestAddValidatorResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	chain, found := k.GetChain(ctx, msg.ChainID)
+	if !found {
+		return nil, sdkerrors.Wrap(types.ErrChainNotFound, msg.ChainID)
+	}
 
-	return &types.MsgRequestAddValidatorResponse{}, nil
+	if chain.LaunchTriggered {
+		return nil, sdkerrors.Wrap(types.ErrTriggeredLaunch, msg.ChainID)
+	}
+
+	content, err := codec.NewAnyWithValue(&types.GenesisValidator{
+		ChainID: msg.ChainID,
+		Address: msg.ValAddress,
+		GenTx:   msg.GenTx,
+		ConsPubKey: msg.ConsPubKey,
+		SelfDelegation: msg.SelfDelegation,
+		Peer: msg.Peer,
+	})
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrCodecNotPacked, msg.String())
+	}
+
+	requestID := k.AppendRequest(ctx, types.Request{
+		ChainID:   msg.ChainID,
+		Creator:   msg.ValAddress,
+		CreatedAt: ctx.BlockTime().Unix(),
+		Content:   content,
+	})
+
+	return &types.MsgRequestAddValidatorResponse{
+		RequestID: requestID,
+	}, nil
 }
