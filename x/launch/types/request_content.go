79ffff144ca47d2dfc667f2e2a3b2f326cfa8017
Danilo Pantani <danpantani@gmail.com>
2021-08-11 19:21:16 -0300

    add request content validation

--- x/launch/types/request_content.go
+++ x/launch/types/request_content.go
@@ -5,10 +5,25 @@ import (
 	"fmt"
 
 	codec "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // RequestContent defines the interface for a request content
-type RequestContent interface{}
+type RequestContent interface {
+	Validate() error
+}
+
+var _ RequestContent = &AccountRemoval{}
+
+// Validate implements AccountRemoval validation
+func (c AccountRemoval) Validate() error {
+	_, err := sdk.AccAddressFromBech32(c.Address)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid account address (%s)", err)
+	}
+	return nil
+}
 
 // UnpackAccountRemoval returns the AccountRemoval structure from the codec unpack
 func (r Request) UnpackAccountRemoval(cdc codec.AnyUnpacker) (*AccountRemoval, error) {
@@ -26,6 +41,41 @@ func (r Request) UnpackAccountRemoval(cdc codec.AnyUnpacker) (*AccountRemoval, e
 		return nil, errors.New("not a accountRemoval request")
 	}
 	return result, nil
+}
+
+var _ RequestContent = &GenesisValidator{}
+
+// Validate implements GenesisValidator validation
+func (c GenesisValidator) Validate() error {
+	_, err := sdk.AccAddressFromBech32(c.Address)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid account address (%s)", err)
+	}
+	_, _, err = ParseChainID(c.ChainID)
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, c.ChainID)
+	}
+
+	if len(c.GenTx) == 0 {
+		return sdkerrors.Wrap(ErrInvalidGenTx, "empty gentx")
+	}
+
+	if len(c.ConsPubKey) == 0 {
+		return sdkerrors.Wrap(ErrInvalidConsPubKey, "empty consensus public key")
+	}
+
+	if !c.SelfDelegation.IsValid() {
+		return sdkerrors.Wrap(ErrInvalidSelfDelegation, "")
+	}
+
+	if c.SelfDelegation.IsZero() {
+		return sdkerrors.Wrap(ErrInvalidSelfDelegation, "self delegation is zero")
+	}
+
+	if c.Peer == "" {
+		return sdkerrors.Wrap(ErrInvalidPeer, "empty peer")
+	}
+	return nil
 }
 
 // UnpackGenesisValidator returns the GenesisValidator structure from the codec unpack
@@ -46,6 +96,25 @@ func (r Request) UnpackGenesisValidator(cdc codec.AnyUnpacker) (*GenesisValidato
 	return result, nil
 }
 
+var _ RequestContent = &GenesisAccount{}
+
+// Validate implements GenesisAccount validation
+func (c GenesisAccount) Validate() error {
+	_, err := sdk.AccAddressFromBech32(c.Address)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid account address (%s)", err)
+	}
+	_, _, err = ParseChainID(c.ChainID)
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, c.ChainID)
+	}
+
+	if !c.Coins.IsValid() || c.Coins.Empty() {
+		return sdkerrors.Wrap(ErrInvalidCoins, c.Address)
+	}
+	return nil
+}
+
 // UnpackGenesisAccount returns the GenesisAccount structure from the codec unpack
 func (r Request) UnpackGenesisAccount(cdc codec.AnyUnpacker) (*GenesisAccount, error) {
 	if r.Content == nil {
@@ -64,6 +133,17 @@ func (r Request) UnpackGenesisAccount(cdc codec.AnyUnpacker) (*GenesisAccount, e
 	return result, nil
 }
 
+var _ RequestContent = &ValidatorRemoval{}
+
+// Validate implements ValidatorRemoval validation
+func (c ValidatorRemoval) Validate() error {
+	_, err := sdk.AccAddressFromBech32(c.ValAddress)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid validator address (%s)", err)
+	}
+	return nil
+}
+
 // UnpackValidatorRemoval returns the ValidatorRemoval structure from the codec unpack
 func (r Request) UnpackValidatorRemoval(cdc codec.AnyUnpacker) (*ValidatorRemoval, error) {
 	if r.Content == nil {
@@ -80,6 +160,26 @@ func (r Request) UnpackValidatorRemoval(cdc codec.AnyUnpacker) (*ValidatorRemova
 		return nil, errors.New("not a validatorRemoval request")
 	}
 	return removeValidator, nil
+}
+
+var _ RequestContent = &VestedAccount{}
+
+// Validate implements VestedAccount validation
+func (c VestedAccount) Validate() error {
+	_, err := sdk.AccAddressFromBech32(c.Address)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid validator address (%s)", err)
+	}
+	_, _, err = ParseChainID(c.ChainID)
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, c.ChainID)
+	}
+
+	if !c.StartingBalance.IsValid() {
+		return sdkerrors.Wrap(ErrInvalidSelfDelegation, c.StartingBalance.String())
+	}
+
+	return nil
 }
 
 // UnpackVestedAccount returns the VestedAccount structure from the codec unpack
