712ae98a172d4d6fa0224ed7a4813b3f965355e8
Danilo Pantani <danpantani@gmail.com>
2021-08-13 04:35:47 -0300

    feat(pkg/error): create `criticalf` error method for additional information (#243)
    * create criticalf error for additional information
    
    * fix import sort and add more description for critical errors

--- pkg/errors/critical.go
+++ pkg/errors/critical.go
@@ -2,15 +2,25 @@ package errors
 
 import (
 	"errors"
+	"fmt"
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 var ErrCritical = errors.New("CRITICAL: the state of the blockchain is inconsistent or an invariant is broken")
 
-// Error handles and/or returns an error in case a critical error has been encountered:
+// Critical handles and/or returns an error in case a critical error has been encountered:
 // - Inconsistent state
 // - Broken invariant
 func Critical(description string) error {
 	return sdkerrors.Wrap(ErrCritical, description)
+}
+
+// Criticalf extends a critical error with additional information.
+//
+// This function works like the Critical function with additional
+// functionality of formatting the input as specified.
+func Criticalf(format string, args ...interface{}) error {
+	desc := fmt.Sprintf(format, args...)
+	return Critical(desc)
 }
