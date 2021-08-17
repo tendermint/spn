b2cc05f15cfe0d6dc8c7d11053143e0876b93752
ltacker <lucas.bertrand.22@gmail.com>
2021-08-17 18:04:29 +0200

    profile genesis tests

--- testutil/sample/profile.go
+++ testutil/sample/profile.go
@@ -1,6 +1,8 @@
 package sample
 
-import profile "github.com/tendermint/spn/x/profile/types"
+import (
+	profile "github.com/tendermint/spn/x/profile/types"
+)
 
 // MsgCreateCoordinator returns a sample MsgCreateCoordinator
 func MsgCreateCoordinator(coordAddress string) profile.MsgCreateCoordinator {
@@ -27,10 +29,56 @@ func ValidatorDescription(desc string) *profile.ValidatorDescription {
 func Coordinator() profile.Coordinator {
 	return profile.Coordinator{
 		Address: AccAddress(),
-		Description: &profile.CoordinatorDescription{
-			Identity: String(10),
-			Website:  String(10),
-			Details:  String(30),
+		Description: CoordinatorDescription(),
+	}
+}
+
+// CoordinatorDescription returns a sample CoordinatorDescription
+func CoordinatorDescription() *profile.CoordinatorDescription {
+	return &profile.CoordinatorDescription{
+		Identity: String(10),
+		Website:  String(10),
+		Details:  String(30),
+	}
+}
+
+// ProfileGenesisState returns a sample genesis state for the profile module
+func ProfileGenesisState() profile.GenesisState {
+	coordAddr1, coordAddr2 := AccAddress(), AccAddress()
+
+	return profile.GenesisState{
+		ValidatorList: []profile.Validator{
+			{
+				AccAddress(),
+				ValidatorDescription(String(10)),
+			},
+			{
+				AccAddress(),
+				ValidatorDescription(String(10)),
+			},
 		},
+		CoordinatorList: []profile.Coordinator{
+			{
+				0,
+				coordAddr1,
+				CoordinatorDescription(),
+			},
+			{
+				1,
+				coordAddr2,
+				CoordinatorDescription(),
+			},
+		},
+		CoordinatorByAddressList: []profile.CoordinatorByAddress{
+			{
+				coordAddr1,
+				0,
+			},
+			{
+				coordAddr2,
+				1,
+			},
+		},
+		CoordinatorCount: 2,
 	}
 }
