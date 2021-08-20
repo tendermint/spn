cfa0086d947b35289a2f4d50872b73b0bcb17647
ltacker <lucas.bertrand.22@gmail.com>
2021-08-20 15:25:25 +0200

    Refactor request content

--- x/launch/types/request_content.go
+++ x/launch/types/request_content.go
@@ -1,16 +1,150 @@
 package types
 
 import (
+	"errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// Validatable defines the interface for a type that can be validated
-type Validatable interface {
-	Validate() error
+func (m RequestContent) Validate() error {
+	switch requestContent := m.Content.(type) {
+	case *RequestContent_GenesisAccount:
+		return requestContent.GenesisAccount.Validate()
+	case *RequestContent_VestedAccount:
+		return requestContent.VestedAccount.Validate()
+	case *RequestContent_GenesisValidator:
+		return requestContent.GenesisValidator.Validate()
+	case *RequestContent_AccountRemoval:
+		return requestContent.AccountRemoval.Validate()
+	case *RequestContent_ValidatorRemoval:
+		return requestContent.ValidatorRemoval.Validate()
+	default:
+		return errors.New("unrecognized request content")
+	}
 }
 
-// NewAccountRemoval returns an InitialGenesis containing a DefaultInitialGenesis
+// NewGenesisAccount returns a RequestContent containing an GenesisAccount
+func NewGenesisAccount(chainID, address string, coins sdk.Coins) RequestContent {
+	return RequestContent{
+		Content: &RequestContent_GenesisAccount{
+			GenesisAccount: &GenesisAccount{
+				ChainID: chainID,
+				Address: address,
+				Coins: coins,
+			},
+		},
+	}
+}
+
+// Validate implements GenesisAccount validation
+func (m GenesisAccount) Validate() error {
+	_, err := sdk.AccAddressFromBech32(m.Address)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid account address (%s)", err)
+	}
+	_, _, err = ParseChainID(m.ChainID)
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, m.ChainID)
+	}
+
+	if !m.Coins.IsValid() || m.Coins.Empty() {
+		return sdkerrors.Wrap(ErrInvalidCoins, m.Address)
+	}
+	return nil
+}
+
+// NewVestedAccount returns a RequestContent containing a VestedAccount
+func NewVestedAccount(chainID, address string, startingBalance sdk.Coins, vestingOptions VestingOptions) RequestContent {
+	return RequestContent{
+		Content: &RequestContent_VestedAccount{
+			VestedAccount: &VestedAccount{
+				ChainID: chainID,
+				Address: address,
+				StartingBalance: startingBalance,
+				VestingOptions: vestingOptions,
+			},
+		},
+	}
+}
+
+// Validate implements VestedAccount validation
+func (m VestedAccount) Validate() error {
+	_, err := sdk.AccAddressFromBech32(m.Address)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid validator address (%s)", err)
+	}
+	_, _, err = ParseChainID(m.ChainID)
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, m.ChainID)
+	}
+
+	if !m.StartingBalance.IsValid() {
+		return sdkerrors.Wrap(ErrInvalidSelfDelegation, m.StartingBalance.String())
+	}
+
+	if err := m.VestingOptions.Validate(); err != nil {
+		return sdkerrors.Wrapf(ErrInvalidVestingOption, err.Error())
+	}
+	return nil
+}
+
+// NewGenesisValidator returns a RequestContent containing a GenesisValidator
+func NewGenesisValidator(
+	chainID,
+	address string,
+	genTx,
+	consPubKey []byte,
+	selfDelegation sdk.Coin,
+	peer string,
+	) RequestContent {
+	return RequestContent{
+		Content: &RequestContent_GenesisValidator{
+			GenesisValidator: &GenesisValidator{
+				ChainID:         chainID,
+				Address:         address,
+				GenTx: genTx,
+				ConsPubKey: consPubKey,
+				SelfDelegation: selfDelegation,
+				Peer: peer,
+			},
+		},
+	}
+}
+
+// Validate implements GenesisValidator validation
+func (m GenesisValidator) Validate() error {
+	_, err := sdk.AccAddressFromBech32(m.Address)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid account address (%s)", err)
+	}
+	_, _, err = ParseChainID(m.ChainID)
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, m.ChainID)
+	}
+
+	if len(m.GenTx) == 0 {
+		return sdkerrors.Wrap(ErrInvalidGenTx, "empty gentx")
+	}
+
+	if len(m.ConsPubKey) == 0 {
+		return sdkerrors.Wrap(ErrInvalidConsPubKey, "empty consensus public key")
+	}
+
+	if !m.SelfDelegation.IsValid() {
+		return sdkerrors.Wrap(ErrInvalidSelfDelegation, "")
+	}
+
+	if m.SelfDelegation.IsZero() {
+		return sdkerrors.Wrap(ErrInvalidSelfDelegation, "self delegation is zero")
+	}
+
+	if m.Peer == "" {
+		return sdkerrors.Wrap(ErrInvalidPeer, "empty peer")
+	}
+	return nil
+}
+
+// NewAccountRemoval returns a RequestContent containing an AccountRemoval
 func NewAccountRemoval(address string) RequestContent {
 	return RequestContent{
 		Content: &RequestContent_AccountRemoval{
@@ -21,102 +155,32 @@ func NewAccountRemoval(address string) RequestContent {
 	}
 }
 
-var _ Validatable = &AccountRemoval{}
-
 // Validate implements AccountRemoval validation
-func (c AccountRemoval) Validate() error {
-	_, err := sdk.AccAddressFromBech32(c.Address)
+func (m AccountRemoval) Validate() error {
+	_, err := sdk.AccAddressFromBech32(m.Address)
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid account address (%s)", err)
 	}
 	return nil
 }
 
-var _ Validatable = &GenesisValidator{}
-
-// Validate implements GenesisValidator validation
-func (c GenesisValidator) Validate() error {
-	_, err := sdk.AccAddressFromBech32(c.Address)
-	if err != nil {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid account address (%s)", err)
+// NewValidatorRemoval returns a RequestContent containing a ValidatorRemoval
+func NewValidatorRemoval(address string) RequestContent {
+	return RequestContent{
+		Content: &RequestContent_ValidatorRemoval{
+			ValidatorRemoval: &ValidatorRemoval{
+				ValAddress: address,
+			},
+		},
 	}
-	_, _, err = ParseChainID(c.ChainID)
-	if err != nil {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, c.ChainID)
-	}
-
-	if len(c.GenTx) == 0 {
-		return sdkerrors.Wrap(ErrInvalidGenTx, "empty gentx")
-	}
-
-	if len(c.ConsPubKey) == 0 {
-		return sdkerrors.Wrap(ErrInvalidConsPubKey, "empty consensus public key")
-	}
-
-	if !c.SelfDelegation.IsValid() {
-		return sdkerrors.Wrap(ErrInvalidSelfDelegation, "")
-	}
-
-	if c.SelfDelegation.IsZero() {
-		return sdkerrors.Wrap(ErrInvalidSelfDelegation, "self delegation is zero")
-	}
-
-	if c.Peer == "" {
-		return sdkerrors.Wrap(ErrInvalidPeer, "empty peer")
-	}
-	return nil
 }
-
-var _ Validatable = &GenesisAccount{}
-
-// Validate implements GenesisAccount validation
-func (c GenesisAccount) Validate() error {
-	_, err := sdk.AccAddressFromBech32(c.Address)
-	if err != nil {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid account address (%s)", err)
-	}
-	_, _, err = ParseChainID(c.ChainID)
-	if err != nil {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, c.ChainID)
-	}
-
-	if !c.Coins.IsValid() || c.Coins.Empty() {
-		return sdkerrors.Wrap(ErrInvalidCoins, c.Address)
-	}
-	return nil
-}
-
-var _ Validatable = &ValidatorRemoval{}
 
 // Validate implements ValidatorRemoval validation
-func (c ValidatorRemoval) Validate() error {
-	_, err := sdk.AccAddressFromBech32(c.ValAddress)
+func (m ValidatorRemoval) Validate() error {
+	_, err := sdk.AccAddressFromBech32(m.ValAddress)
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid validator address (%s)", err)
 	}
 	return nil
 }
 
-
-var _ Validatable = &VestedAccount{}
-
-// Validate implements VestedAccount validation
-func (c VestedAccount) Validate() error {
-	_, err := sdk.AccAddressFromBech32(c.Address)
-	if err != nil {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid validator address (%s)", err)
-	}
-	_, _, err = ParseChainID(c.ChainID)
-	if err != nil {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidChainID, c.ChainID)
-	}
-
-	if !c.StartingBalance.IsValid() {
-		return sdkerrors.Wrap(ErrInvalidSelfDelegation, c.StartingBalance.String())
-	}
-
-	if err := c.VestingOptions.Validate(); err != nil {
-		return sdkerrors.Wrapf(ErrInvalidVestingOption, err.Error())
-	}
-	return nil
-}
