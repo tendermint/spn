de19c4085b47241cd6dfc4e47d7505fc5bb64dfd
ltacker <lucas.bertrand.22@gmail.com>
2021-09-02 13:55:55 +0200

    import cycle

--- x/campaign/keeper/msg_server_test.go
+++ x/campaign/keeper/msg_server_test.go
@@ -1,4 +1,4 @@
-package keeper
+package keeper_test
 
 import (
 	"context"
@@ -6,10 +6,11 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	testkeeper "github.com/tendermint/spn/testutil/keeper"
+	"github.com/tendermint/spn/x/campaign/keeper"
 	"github.com/tendermint/spn/x/campaign/types"
 )
 
 func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-	keeper, ctx := testkeeper.Campaign(t)
-	return NewMsgServerImpl(*keeper), sdk.WrapSDKContext(ctx)
+	k, ctx := testkeeper.Campaign(t)
+	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
 }
