0133266cb90397172d953bddef40e1242c5219e5
ltacker <lucas.bertrand.22@gmail.com>
2021-09-06 13:55:41 +0200

    Sample

--- testutil/sample/campaign.go
+++ testutil/sample/campaign.go
@@ -2,6 +2,11 @@ package sample
 
 import campaign "github.com/tendermint/spn/x/campaign/types"
 
+// Shares returns a sample shares
+func Shares() campaign.Shares {
+	return campaign.NewSharesFromCoins(Coins())
+}
+
 // CampaignGenesisState returns a sample genesis state for the campaign module
 func CampaignGenesisState() campaign.GenesisState {
 	return campaign.GenesisState{}
