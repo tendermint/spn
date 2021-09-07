515339915c23ce6daf22000cf285839c324033e3
ltacker <lucas.bertrand.22@gmail.com>
2021-09-07 11:26:04 +0200

    SAmple

--- testutil/sample/campaign.go
+++ testutil/sample/campaign.go
@@ -2,6 +2,16 @@ package sample
 
 import campaign "github.com/tendermint/spn/x/campaign/types"
 
+// CampaignName returns a sample campaign name
+func CampaignName() string {
+	return String(20)
+}
+
+// Campaign returns a sample campaign
+func Campaign() campaign.Campaign {
+	return campaign.NewCampaign(CampaignName(), Uint64(), Coins(), false)
+}
+
 // CampaignGenesisState returns a sample genesis state for the campaign module
 func CampaignGenesisState() campaign.GenesisState {
 	return campaign.GenesisState{}
