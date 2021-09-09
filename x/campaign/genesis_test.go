18c3a36e20168ee0f77e4fca715dc7c2c5862840
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2021-09-09 10:19:50 +0200

    fix: add placeholders to support genesis test scaffolding (#293)
    * fix: support genesis test
    
    * Update genesis tests

--- x/campaign/genesis_test.go
+++ x/campaign/genesis_test.go
@@ -9,15 +9,21 @@ import (
 	"github.com/tendermint/spn/x/campaign"
 )
 
+/*
+// We use a genesis template from sample package, therefore this placeholder is not used
+// this line is used by starport scaffolding # genesis/test/state
+*/
+
 func TestGenesis(t *testing.T) {
 	keeper, ctx := testkeeper.Campaign(t)
 
-	original := sample.CampaignGenesisState()
-	campaign.InitGenesis(ctx, *keeper, original)
+	genesisState := sample.CampaignGenesisState()
+	campaign.InitGenesis(ctx, *keeper, genesisState)
 	got := *campaign.ExportGenesis(ctx, *keeper)
 
-	require.Len(t, got.CampaignList, len(original.CampaignList))
-	require.Subset(t, original.CampaignList, got.CampaignList)
+	require.Len(t, got.CampaignList, len(genesisState.CampaignList))
+	require.Subset(t, genesisState.CampaignList, got.CampaignList)
+	require.Equal(t, genesisState.CampaignCount, got.CampaignCount)
 
-	require.Equal(t, original.CampaignCount, got.CampaignCount)
+	// this line is used by starport scaffolding # genesis/test/assert
 }
