2c3204c89b10d8ad8fb669448353c2f9a9777fd5
ltacker <lucas.bertrand.22@gmail.com>
2021-09-10 10:20:45 +0200

    Message logic test

--- x/campaign/keeper/msg_server_test.go
+++ x/campaign/keeper/msg_server_test.go
@@ -1,16 +1,28 @@
 package keeper_test
 
 import (
-	"context"
 	"testing"
 
+	profilekeeper "github.com/tendermint/spn/x/profile/keeper"
+	profiletypes "github.com/tendermint/spn/x/profile/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	testkeeper "github.com/tendermint/spn/testutil/keeper"
 	"github.com/tendermint/spn/x/campaign/keeper"
 	"github.com/tendermint/spn/x/campaign/types"
 )
 
-func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-	k, ctx := testkeeper.Campaign(t)
-	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+func setupMsgServer(t testing.TB) (
+	*keeper.Keeper,
+	*profilekeeper.Keeper,
+	types.MsgServer,
+	profiletypes.MsgServer,
+	sdk.Context,
+) {
+	campaignKeeper, _, profileKeeper, ctx := testkeeper.AllKeepers(t)
+
+	return campaignKeeper,
+		profileKeeper,
+		keeper.NewMsgServerImpl(*campaignKeeper),
+		profilekeeper.NewMsgServerImpl(*profileKeeper),
+		ctx
 }
