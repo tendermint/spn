6740b80bb8afb93c25bc737797880f8082f1ab1b
ltacker <lucas.bertrand.22@gmail.com>
2021-09-10 16:45:51 +0200

    Lint

--- x/campaign/types/genesis.go
+++ x/campaign/types/genesis.go
@@ -10,8 +10,8 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		CampaignList:       []Campaign{},
-		CampaignChainsList: []CampaignChains{},
+		CampaignList:              []Campaign{},
+		CampaignChainsList:        []CampaignChains{},
 		MainnetAccountList:        []MainnetAccount{},
 		MainnetVestingAccountList: []MainnetVestingAccount{},
 		// this line is used by starport scaffolding # genesis/types/default
