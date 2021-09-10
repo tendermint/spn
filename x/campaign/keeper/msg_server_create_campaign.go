9e83d484fb18779be5510381aaac7c5e31fb0799
ltacker <lucas.bertrand.22@gmail.com>
2021-09-10 09:53:55 +0200

    Message logic

--- x/campaign/keeper/msg_server_create_campaign.go
+++ x/campaign/keeper/msg_server_create_campaign.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	profiletypes "github.com/tendermint/spn/x/profile/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tendermint/spn/x/campaign/types"
@@ -10,8 +12,15 @@ import (
 func (k msgServer) CreateCampaign(goCtx context.Context, msg *types.MsgCreateCampaign) (*types.MsgCreateCampaignResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Get the coordinator ID
+	coordinatorID, found := k.profileKeeper.CoordinatorIDFromAddress(ctx, msg.Coordinator)
+	if !found {
+		return nil, sdkerrors.Wrap(profiletypes.ErrCoordAddressNotFound, msg.Coordinator)
+	}
 
-	return &types.MsgCreateCampaignResponse{}, nil
+	// Append the new campaign
+	campaign := types.NewCampaign(0, msg.CampaignName, coordinatorID, msg.TotalSupply, msg.DynamicShares)
+	campaignID := k.AppendCampaign(ctx, campaign)
+
+	return &types.MsgCreateCampaignResponse{CampaignID: campaignID}, nil
 }
