d8a65d24a69806b9ef77b9695a5df66f4818bac8
ltacker <lucas.bertrand.22@gmail.com>
2021-09-10 17:42:32 +0200

    Type tests

--- x/campaign/types/message_update_total_shares_test.go
+++ x/campaign/types/message_update_total_shares_test.go
@@ -1,30 +1,66 @@
-package types
+package types_test
 
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/spn/testutil/sample"
+	"github.com/tendermint/spn/x/campaign/types"
 )
 
 func TestMsgUpdateTotalShares_ValidateBasic(t *testing.T) {
 	tests := []struct {
 		name string
-		msg  MsgUpdateTotalShares
+		msg  types.MsgUpdateTotalShares
 		err  error
 	}{
 		{
+			name: "valid address",
+			msg: types.MsgUpdateTotalShares{
+				Coordinator: sample.AccAddress(),
+				CampaignID:  0,
+				TotalShares: sample.Shares(),
+			},
+		},
+		{
 			name: "invalid address",
-			msg: MsgUpdateTotalShares{
+			msg: types.MsgUpdateTotalShares{
 				Coordinator: "invalid_address",
+				CampaignID:  0,
+				TotalShares: sample.Shares(),
 			},
 			err: sdkerrors.ErrInvalidAddress,
-		}, {
-			name: "valid address",
-			msg: MsgUpdateTotalShares{
+		},
+		{
+			name: "invalid coins for total shares",
+			msg: types.MsgUpdateTotalShares{
 				Coordinator: sample.AccAddress(),
+				CampaignID:  0,
+				TotalShares: types.NewSharesFromCoins(invalidCoins),
 			},
+			err: types.ErrInvalidShares,
+		},
+		{
+			name: "empty total shares",
+			msg: types.MsgUpdateTotalShares{
+				Coordinator: sample.AccAddress(),
+				CampaignID:  0,
+				TotalShares: types.EmptyShares(),
+			},
+			err: types.ErrInvalidShares,
+		},
+		{
+			name: "invalid shares prefix for total shares",
+			msg: types.MsgUpdateTotalShares{
+				Coordinator: sample.AccAddress(),
+				CampaignID:  0,
+				TotalShares: types.Shares(sdk.NewCoins(
+					sdk.NewCoin("foo", sdk.NewInt(100)),
+				)),
+			},
+			err: types.ErrInvalidShares,
 		},
 	}
 	for _, tt := range tests {
