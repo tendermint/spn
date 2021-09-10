dcda038bce2ad8ca0d6b51fdae46c8906c30926e
ltacker <lucas.bertrand.22@gmail.com>
2021-09-10 11:49:19 +0200

    Update CLI

--- x/campaign/client/cli/tx_update_total_supply.go
+++ x/campaign/client/cli/tx_update_total_supply.go
@@ -1,26 +1,27 @@
 package cli
 
 import (
-	"strconv"
-
-	"github.com/spf13/cast"
-	"github.com/spf13/cobra"
-
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/spf13/cast"
+	"github.com/spf13/cobra"
 	"github.com/tendermint/spn/x/campaign/types"
 )
 
-var _ = strconv.Itoa(0)
-
 func CmdUpdateTotalSupply() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update-total-supply [campaign-id]",
+		Use:   "update-total-supply [campaign-id] [total-shares]",
 		Short: "Update the total of the mainnet of a campaign",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			argCampaignID, err := cast.ToUint64E(args[0])
+			campaignID, err := cast.ToUint64E(args[0])
+			if err != nil {
+				return err
+			}
+
+			totalShares, err := sdk.ParseCoinsNormalized(args[1])
 			if err != nil {
 				return err
 			}
@@ -32,7 +33,8 @@ func CmdUpdateTotalSupply() *cobra.Command {
 
 			msg := types.NewMsgUpdateTotalSupply(
 				clientCtx.GetFromAddress().String(),
-				argCampaignID,
+				campaignID,
+				totalShares,
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
