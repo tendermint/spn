dec654d30d28956d9946a0a4d4c241b39b2c87d2
ltacker <lucas.bertrand.22@gmail.com>
2021-09-10 11:22:13 +0200

    Complete message type

--- x/campaign/types/errors.go
+++ x/campaign/types/errors.go
@@ -8,5 +8,5 @@ import (
 
 // x/campaign module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrInvalidTotalSupply  = sdkerrors.Register(ModuleName, 2, "invalid total supply")
 )
--- x/campaign/types/message_update_total_supply.go
+++ x/campaign/types/message_update_total_supply.go
@@ -7,10 +7,11 @@ import (
 
 var _ sdk.Msg = &MsgUpdateTotalSupply{}
 
-func NewMsgUpdateTotalSupply(coordinator string, campaignID uint64) *MsgUpdateTotalSupply {
+func NewMsgUpdateTotalSupply(coordinator string, campaignID uint64, totalSupply sdk.Coins) *MsgUpdateTotalSupply {
 	return &MsgUpdateTotalSupply{
 		Coordinator: coordinator,
 		CampaignID:  campaignID,
+		TotalSupply: totalSupply,
 	}
 }
 
@@ -40,5 +41,14 @@ func (msg *MsgUpdateTotalSupply) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid coordinator address (%s)", err)
 	}
+
+	if !msg.TotalSupply.IsValid() {
+		return sdkerrors.Wrap(ErrInvalidTotalSupply, "total supply is not a valid Coins object")
+	}
+
+	if msg.TotalSupply.Empty() {
+		return sdkerrors.Wrap(ErrInvalidTotalSupply, "total supply is empty")
+	}
+
 	return nil
 }
