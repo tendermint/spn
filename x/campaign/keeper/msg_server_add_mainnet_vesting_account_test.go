a865e7fb6e7c01c6338ec39698cea3496f10cf05
Danilo Pantani <danpantani@gmail.com>
2021-09-22 15:49:23 -0300

    add test case for already exist address for campaign

--- x/campaign/keeper/msg_server_add_mainnet_vesting_account_test.go
+++ x/campaign/keeper/msg_server_add_mainnet_vesting_account_test.go
@@ -12,15 +12,19 @@ import (
 
 func TestMsgAddMainnetVestingAccount(t *testing.T) {
 	var (
-		addr                                               = sample.AccAddress()
-		coordAddr1                                         = sample.AccAddress()
-		coordAddr2                                         = sample.AccAddress()
-		coordAddr3                                         = sample.AccAddress()
+		addr1 = sample.AccAddress()
+		addr2 = sample.AccAddress()
+
+		coordAddr1 = sample.AccAddress()
+		coordAddr2 = sample.AccAddress()
+		coordAddr3 = sample.AccAddress()
+
+		campaign1 = sample.Campaign(0)
+		campaign2 = sample.Campaign(1)
+		campaign3 = sample.Campaign(2)
+
 		campaignKeeper, _, campaignSrv, profileSrv, sdkCtx = setupMsgServer(t)
 		ctx                                                = sdk.WrapSDKContext(sdkCtx)
-		campaign1                                          = sample.Campaign(0)
-		campaign2                                          = sample.Campaign(1)
-		campaign3                                          = sample.Campaign(2)
 	)
 
 	// create shares
@@ -44,6 +48,10 @@ func TestMsgAddMainnetVestingAccount(t *testing.T) {
 	campaign1.AllocatedShares = allocatedShares
 	campaign1.TotalShares = totalShares
 
+	campaignKeeper.SetMainnetVestingAccount(
+		sdkCtx,
+		sample.MainnetVestingAccount(campaign1.Id, addr2),
+	)
 	res2, err := profileSrv.CreateCoordinator(ctx, &profiletypes.MsgCreateCoordinator{
 		Address:     coordAddr2,
 		Description: sample.CoordinatorDescription(),
@@ -74,69 +82,95 @@ func TestMsgAddMainnetVestingAccount(t *testing.T) {
 		{
 			name: "invalid campaign id",
 			msg: types.MsgAddMainnetVestingAccount{
-				Coordinator: coordAddr1,
-				CampaignID:  100,
-				Address:     addr,
-				Shares:      sample.Shares(),
+				Coordinator:    coordAddr1,
+				CampaignID:     100,
+				Address:        addr1,
+				Shares:         sample.Shares(),
+				VestingOptions: sample.ShareVestingOptions(),
 			},
 			err: types.ErrCampaignNotFound,
 		},
 		{
 			name: "invalid coordinator address",
 			msg: types.MsgAddMainnetVestingAccount{
-				Coordinator: addr,
-				CampaignID:  campaign1.Id,
-				Address:     addr,
-				Shares:      sample.Shares(),
+				Coordinator:    addr1,
+				CampaignID:     campaign1.Id,
+				Address:        addr1,
+				Shares:         sample.Shares(),
+				VestingOptions: sample.ShareVestingOptions(),
 			},
 			err: profiletypes.ErrCoordAddressNotFound,
 		},
 		{
 			name: "invalid coordinator id",
 			msg: types.MsgAddMainnetVestingAccount{
-				Coordinator: coordAddr2,
-				CampaignID:  campaign1.Id,
-				Address:     addr,
-				Shares:      sample.Shares(),
+				Coordinator:    coordAddr2,
+				CampaignID:     campaign1.Id,
+				Address:        addr1,
+				Shares:         sample.Shares(),
+				VestingOptions: sample.ShareVestingOptions(),
 			},
 			err: profiletypes.ErrCoordInvalid,
 		},
 		{
 			name: "campaign already in mainnet",
 			msg: types.MsgAddMainnetVestingAccount{
-				Coordinator: coordAddr2,
-				CampaignID:  campaign2.Id,
-				Address:     addr,
-				Shares:      sample.Shares(),
+				Coordinator:    coordAddr2,
+				CampaignID:     campaign2.Id,
+				Address:        addr1,
+				Shares:         sample.Shares(),
+				VestingOptions: sample.ShareVestingOptions(),
 			},
 			err: types.ErrMainnetInitialized,
 		},
 		{
 			name: "allocated shares greater them total shares",
 			msg: types.MsgAddMainnetVestingAccount{
-				Coordinator: coordAddr3,
-				CampaignID:  campaign3.Id,
-				Address:     addr,
-				Shares:      share1,
+				Coordinator:    coordAddr3,
+				CampaignID:     campaign3.Id,
+				Address:        addr1,
+				Shares:         share1,
+				VestingOptions: sample.ShareVestingOptions(),
 			},
 			err: types.ErrTotalShareLimit,
 		},
 		{
-			name: "valid message",
+			name: "create new account with shares",
 			msg: types.MsgAddMainnetVestingAccount{
-				Coordinator: coordAddr1,
-				CampaignID:  campaign1.Id,
-				Address:     addr,
-				Shares:      share2,
+				Coordinator:    coordAddr1,
+				CampaignID:     campaign1.Id,
+				Address:        addr1,
+				Shares:         share2,
+				VestingOptions: sample.ShareVestingOptions(),
+			},
+		},
+		{
+			name: "update existing account shares",
+			msg: types.MsgAddMainnetVestingAccount{
+				Coordinator:    coordAddr1,
+				CampaignID:     campaign1.Id,
+				Address:        addr2,
+				Shares:         share1,
+				VestingOptions: sample.ShareVestingOptions(),
 			},
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			var tmpCampaign types.Campaign
+			var (
+				accountExists bool
+				tmpAccount    types.MainnetVestingAccount
+				tmpCampaign   types.Campaign
+			)
 			if tc.err == nil {
 				var found bool
 				tmpCampaign, found = campaignKeeper.GetCampaign(sdkCtx, tc.msg.CampaignID)
 				require.True(t, found)
+
+				tmpAccount, accountExists = campaignKeeper.GetMainnetVestingAccount(
+					sdkCtx,
+					tc.msg.CampaignID,
+					tc.msg.Address,
+				)
 			}
 			_, err := campaignSrv.AddMainnetVestingAccount(ctx, &tc.msg)
 			if tc.err != nil {
@@ -151,11 +185,27 @@ func TestMsgAddMainnetVestingAccount(t *testing.T) {
 			campaign, found := campaignKeeper.GetCampaign(sdkCtx, tc.msg.CampaignID)
 			require.True(t, found)
 
-			tmpShare, err := types.DecreaseShares(campaign.AllocatedShares, account.Shares)
-			require.NoError(t, err)
+			if accountExists {
+				shares, err := types.DecreaseShares(account.Shares, tmpAccount.Shares)
+				require.NoError(t, err)
+				equal := types.IsEqualShares(shares, tc.msg.Shares)
+				require.True(t, equal)
+			} else {
+				tmpShare, err := types.DecreaseShares(campaign.AllocatedShares, account.Shares)
+				require.NoError(t, err)
 
-			equal := types.IsEqualShares(tmpCampaign.AllocatedShares, tmpShare)
-			require.True(t, equal)
+				switch vestionOptions := tc.msg.VestingOptions.Options.(type) {
+				case *types.ShareVestingOptions_DelayedVesting:
+					vestingShares := vestionOptions.DelayedVesting.Vesting
+					tmpShare, err = types.DecreaseShares(tmpShare, vestingShares)
+					require.NoError(t, err)
+				default:
+					t.Fatal("invalid vesting options type")
+				}
+
+				equal := types.IsEqualShares(tmpCampaign.AllocatedShares, tmpShare)
+				require.True(t, equal)
+			}
 		})
 	}
 }
