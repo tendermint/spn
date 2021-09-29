bb14649327016066c0f30aeaaf26e970e0368670
Danilo Pantani <danpantani@gmail.com>
2021-09-29 10:17:36 -0300

    feat(campaign): allows to add vesting options (#305)
    * add campaign mainnet vesting account
    
    * fix import sort lint
    
    * check if account already exists
    
    * sum the vestiong options shares
    
    * add test case for already exist address for campaign
    
    * decrease and increase allocated shares instead only overwrite
    
    * apply PR discussions
    
    * rename `MsgAddMainnetVestingAccount` to `AddVestingOptions`
    
    * compare vesting options for `TestMsgAddVestingOptions` method
    
    * rename `shares` field to `startingShares`
    
    * improve test readability
    
    * add more context to the critical error for `GetTotalShares()`
    
    * fix field name from `Shares` to `StartingShares`
    
    * remove unecessary test case

---
+++ x/campaign/client/cli/tx_add_vesting_options.go
@@ -0,0 +1,64 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/spf13/cast"
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/tendermint/spn/x/campaign/types"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdAddVestingOptions() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "add-vesting-options [campaign-id] [address] [starting-shares] [vesting-shares] [vesting-end-time]",
+		Short: "Add a mainnet vesting account",
+		Args:  cobra.ExactArgs(5),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			argCampaignID, err := cast.ToUint64E(args[0])
+			if err != nil {
+				return err
+			}
+
+			argAddress := args[1]
+
+			startingShares, err := types.NewShares(args[2])
+			if err != nil {
+				return err
+			}
+
+			vestingShares, err := types.NewShares(args[3])
+			if err != nil {
+				return err
+			}
+			endTime, _ := strconv.ParseUint(args[4], 10, 64)
+			delayedVesting := *types.NewShareDelayedVesting(vestingShares, int64(endTime))
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgAddVestingOptions(
+				argCampaignID,
+				clientCtx.GetFromAddress().String(),
+				argAddress,
+				startingShares,
+				delayedVesting,
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
---
+++ x/campaign/keeper/msg_server_add_vesting_options.go
@@ -0,0 +1,78 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	spnerrors "github.com/tendermint/spn/pkg/errors"
+	"github.com/tendermint/spn/x/campaign/types"
+	profiletypes "github.com/tendermint/spn/x/profile/types"
+)
+
+func (k msgServer) AddVestingOptions(goCtx context.Context, msg *types.MsgAddVestingOptions) (*types.MsgAddVestingOptionsResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	campaign, found := k.GetCampaign(ctx, msg.CampaignID)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrCampaignNotFound, "%v", msg.CampaignID)
+	}
+
+	// Get the coordinator ID
+	coordinatorID, found := k.profileKeeper.CoordinatorIDFromAddress(ctx, msg.Coordinator)
+	if !found {
+		return nil, sdkerrors.Wrap(profiletypes.ErrCoordAddressNotFound, msg.Coordinator)
+	}
+	if campaign.CoordinatorID != coordinatorID {
+		return nil, sdkerrors.Wrap(profiletypes.ErrCoordInvalid, fmt.Sprintf(
+			"coordinator of the campaign is %v",
+			campaign.CoordinatorID,
+		))
+	}
+
+	if campaign.MainnetInitialized {
+		return nil, sdkerrors.Wrapf(types.ErrMainnetInitialized, "%v", msg.CampaignID)
+	}
+
+	// check if the account already exists
+	oldAccount, foundAcc := k.GetMainnetVestingAccount(ctx, campaign.Id, msg.Address)
+	if foundAcc {
+		// if yes, remove the allocated share
+		totalShares, err := oldAccount.GetTotalShares()
+		if err != nil {
+			return nil, spnerrors.Critical(err.Error())
+		}
+		campaign.AllocatedShares, err = types.DecreaseShares(campaign.AllocatedShares, totalShares)
+		if err != nil {
+			return nil, spnerrors.Critical("campaign allocated shares is lower than all shares")
+		}
+	}
+
+	account := types.MainnetVestingAccount{
+		CampaignID:     campaign.Id,
+		Address:        msg.Address,
+		Shares:         msg.StartingShares,
+		VestingOptions: msg.VestingOptions,
+	}
+
+	// get the VestingOption and account shares
+	totalShares, err := account.GetTotalShares()
+	if err != nil {
+		return nil, spnerrors.Criticalf(
+			"fail to get the account and vesting option shares: %s",
+			err.Error(),
+		)
+	}
+
+	// increase the campaign shares
+	campaign.AllocatedShares = types.IncreaseShares(campaign.AllocatedShares, totalShares)
+	if types.IsTotalSharesReached(campaign.AllocatedShares, campaign.TotalShares) {
+		return nil, sdkerrors.Wrapf(types.ErrTotalSharesLimit, "%v", msg.CampaignID)
+	}
+
+	k.SetCampaign(ctx, campaign)
+	k.SetMainnetVestingAccount(ctx, account)
+
+	return &types.MsgAddVestingOptionsResponse{}, nil
+}
