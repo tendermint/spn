66c0f3046b40a2d7ab328b319b16c6598c4628b3
ltacker <lucas.bertrand.22@gmail.com>
2021-10-01 11:03:12 +0200

    Implement message

--- x/campaign/keeper/msg_server_mint_vouchers.go
+++ x/campaign/keeper/msg_server_mint_vouchers.go
@@ -2,16 +2,58 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	spnerrors "github.com/tendermint/spn/pkg/errors"
 	"github.com/tendermint/spn/x/campaign/types"
+	profiletypes "github.com/tendermint/spn/x/profile/types"
 )
 
 func (k msgServer) MintVouchers(goCtx context.Context, msg *types.MsgMintVouchers) (*types.MsgMintVouchersResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	campaign, found := k.GetCampaign(ctx, msg.CampaignID)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrCampaignNotFound, "%d", msg.CampaignID)
+	}
+
+	// Get the coordinator ID
+	coordinatorID, found := k.profileKeeper.CoordinatorIDFromAddress(ctx, msg.Coordinator)
+	if !found {
+		return nil, sdkerrors.Wrap(profiletypes.ErrCoordAddressNotFound, msg.Coordinator)
+	}
+	if campaign.CoordinatorID != coordinatorID {
+		return nil, sdkerrors.Wrap(profiletypes.ErrCoordInvalid, fmt.Sprintf(
+			"coordinator of the campaign is %v",
+			campaign.CoordinatorID,
+		))
+	}
+
+	// Increase the campaign shares
+	campaign.AllocatedShares = types.IncreaseShares(campaign.AllocatedShares, msg.Shares)
+	if types.IsTotalSharesReached(campaign.AllocatedShares, campaign.TotalShares) {
+		return nil, sdkerrors.Wrapf(types.ErrTotalSharesLimit, "%v", msg.CampaignID)
+	}
+
+	// Mint vouchers to the coordinator account
+	vouchers, err := types.SharesToVouchers(msg.Shares, msg.CampaignID)
+	if err != nil {
+		return nil, spnerrors.Criticalf("verified shares are invalid %s", err.Error())
+	}
+	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, vouchers); err != nil {
+		return nil, sdkerrors.Wrap(types.ErrVouchersMinting, err.Error())
+	}
+
+	receiver, err := sdk.AccAddressFromBech32(msg.Coordinator)
+	if err != nil {
+		return nil, spnerrors.Criticalf("can't parse coordinator address %s", err.Error())
+	}
+
+	if err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, receiver, vouchers); err != nil {
+		return nil, spnerrors.Criticalf("can't send minted coins %s", err.Error())
+	}
 
 	return &types.MsgMintVouchersResponse{}, nil
 }
--- x/campaign/types/expected_keepers.go
+++ x/campaign/types/expected_keepers.go
@@ -18,7 +18,10 @@ type LaunchKeeper interface {
 }
 
 type BankKeeper interface {
-	// Methods imported from bank should be defined here
+	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 }
 
 type ProfileKeeper interface {
