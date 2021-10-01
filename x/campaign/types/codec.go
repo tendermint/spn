770cab1a57fe674f09b76b1dbdb9a2ba35e9553e
ltacker <lucas.bertrand.22@gmail.com>
2021-10-01 10:37:25 +0200

    Quality

--- x/campaign/types/codec.go
+++ x/campaign/types/codec.go
@@ -19,11 +19,9 @@ func RegisterCodec(cdc *codec.LegacyAmino) {
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	registry.RegisterImplementations((*sdk.Msg)(nil),
-		&MsgUnredeemVouchers{},
-	)
 	// this line is used by starport scaffolding # 3
 	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgUnredeemVouchers{},
 		&MsgInitializeMainnet{},
 		&MsgUpdateTotalShares{},
 		&MsgUpdateTotalSupply{},
