b8dd52ee02860d14bda5a37bb6bafeb0af8052ff
ltacker <lucas.bertrand.22@gmail.com>
2021-10-04 14:21:21 +0200

    Sim interfaces

---
+++ x/profile/module_simulation.go
@@ -0,0 +1,31 @@
+package profile
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"math/rand"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+)
+
+// GenerateGenesisState creates a randomized GenState of the module
+func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
+
+}
+
+// ProposalContents doesn't return any content functions for governance proposals
+func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalContent {
+	return nil
+}
+
+// RandomizedParams creates randomized  param changes for the simulator
+func (am AppModule) RandomizedParams(r *rand.Rand) []simtypes.ParamChange {
+
+}
+
+// RegisterStoreDecoder registers a decoder
+func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
+
+// WeightedOperations returns the all the gov module operations with their respective weights.
+func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+
+}
