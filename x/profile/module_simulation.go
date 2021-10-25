e6cccf271b809304baa2e95db523add9ae19d091
ltacker <lucas.bertrand.22@gmail.com>
2021-10-25 14:23:56 +0200

    Add weight generate

--- x/profile/module_simulation.go
+++ x/profile/module_simulation.go
@@ -13,12 +13,19 @@ import (
 )
 
 const (
-	weightMsgUpdateValidatorDescription   = 50
-	weightMsgDeleteValidator              = 10
-	weightMsgCreateCoordinator            = 50
-	weightMsgUpdateCoordinatorDescription = 20
-	weightMsgUpdateCoordinatorAddress     = 20
-	weightMsgDeleteCoordinator            = 10
+	defaultWeightMsgUpdateValidatorDescription   = 50
+	defaultWeightMsgDeleteValidator              = 10
+	defaultWeightMsgCreateCoordinator            = 50
+	defaultWeightMsgUpdateCoordinatorDescription = 20
+	defaultWeightMsgUpdateCoordinatorAddress     = 20
+	defaultWeightMsgDeleteCoordinator            = 10
+
+	opWeightMsgUpdateValidatorDescription   = "op_weight_msg_update_validator_description"
+	opWeightMsgDeleteValidator              = "op_weight_msg_delete_validator"
+	opWeightMsgCreateCoordinator            = "op_weight_msg_create_coordinator"
+	opWeightMsgUpdateCoordinatorDescription = "op_weight_msg_update_coordinator_description"
+	opWeightMsgUpdateCoordinatorAddress     = "op_weight_msg_update_coordinator_address"
+	opWeightMsgDeleteCoordinator            = "op_weight_msg_delete_coordinator"
 )
 
 // GenerateGenesisState creates a randomized GenState of the module
@@ -45,7 +52,49 @@ func (am AppModule) RandomizedParams(_ *rand.Rand) []simtypes.ParamChange {
 func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
 
 // WeightedOperations returns the all the gov module operations with their respective weights.
-func (am AppModule) WeightedOperations(_ module.SimulationState) []simtypes.WeightedOperation {
+func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+	var (
+		weightMsgUpdateValidatorDescription   int
+		weightMsgDeleteValidator              int
+		weightMsgCreateCoordinator            int
+		weightMsgUpdateCoordinatorDescription int
+		weightMsgUpdateCoordinatorAddress     int
+		weightMsgDeleteCoordinator            int
+	)
+
+	appParams := simState.AppParams
+	cdc := simState.Cdc
+	appParams.GetOrGenerate(cdc, opWeightMsgUpdateValidatorDescription, &weightMsgUpdateValidatorDescription, nil,
+		func(_ *rand.Rand) {
+			weightMsgUpdateValidatorDescription = defaultWeightMsgUpdateValidatorDescription
+		},
+	)
+	appParams.GetOrGenerate(cdc, opWeightMsgDeleteValidator, &weightMsgDeleteValidator, nil,
+		func(_ *rand.Rand) {
+			weightMsgDeleteValidator = defaultWeightMsgDeleteValidator
+		},
+	)
+	appParams.GetOrGenerate(cdc, opWeightMsgCreateCoordinator, &weightMsgCreateCoordinator, nil,
+		func(_ *rand.Rand) {
+			weightMsgCreateCoordinator = defaultWeightMsgCreateCoordinator
+		},
+	)
+	appParams.GetOrGenerate(cdc, opWeightMsgUpdateCoordinatorDescription, &weightMsgUpdateCoordinatorDescription, nil,
+		func(_ *rand.Rand) {
+			weightMsgUpdateCoordinatorDescription = defaultWeightMsgUpdateCoordinatorDescription
+		},
+	)
+	appParams.GetOrGenerate(cdc, opWeightMsgUpdateCoordinatorAddress, &weightMsgUpdateCoordinatorAddress, nil,
+		func(_ *rand.Rand) {
+			weightMsgUpdateCoordinatorAddress = defaultWeightMsgUpdateCoordinatorAddress
+		},
+	)
+	appParams.GetOrGenerate(cdc, opWeightMsgDeleteCoordinator, &weightMsgDeleteCoordinator, nil,
+		func(_ *rand.Rand) {
+			weightMsgDeleteCoordinator = defaultWeightMsgDeleteCoordinator
+		},
+	)
+
 	return []simtypes.WeightedOperation{
 		simulation.NewWeightedOperation(
 			weightMsgUpdateValidatorDescription,
