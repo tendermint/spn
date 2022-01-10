99011fb9e2a19d5e3af7e8b4fb726ac207821782
ltacker <lucas.bertrand.22@gmail.com>
2022-01-10 15:53:00 +0100

    Consensus client state proto

--- x/monitoringc/keeper/msg_server_create_client.go
+++ x/monitoringc/keeper/msg_server_create_client.go
@@ -1,17 +1,33 @@
 package keeper
 
 import (
+	"bytes"
 	"context"
+	tmtypes "github.com/tendermint/tendermint/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	ibctmtypes "github.com/cosmos/ibc-go/modules/light-clients/07-tendermint/types"
 	"github.com/tendermint/spn/x/monitoringc/types"
 )
 
 func (k msgServer) CreateClient(goCtx context.Context, msg *types.MsgCreateClient) (*types.MsgCreateClientResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
-	
-	return &types.MsgCreateClientResponse{}, nil
+	var clientState ibctmtypes.ClientState
+	var consensusState ibctmtypes.ConsensusState
+
+	// create the client from IBC keeper
+	clientID, err := k.clientKeeper.CreateClient(ctx, &clientState, &consensusState)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.MsgCreateClientResponse{
+		ClientID: clientID,
+	}, nil
+}
+
+func checkValidatorSet(valSet tmtypes.ValidatorSet, consensusState ibctmtypes.ConsensusState) bool {
+	valHash := consensusState.NextValidatorsHash
+	return !bytes.Equal(valHash.Bytes(), valSet.Hash())
 }
