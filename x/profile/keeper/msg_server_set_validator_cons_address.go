c75d26e39fa691f94bd8feb5ab4d94c298dd8492
Danilo Pantani <danpantani@gmail.com>
2022-01-18 12:13:56 -0300

    add msg logic

--- x/profile/keeper/msg_server_set_validator_cons_address.go
+++ x/profile/keeper/msg_server_set_validator_cons_address.go
@@ -5,7 +5,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	spnerrors "github.com/tendermint/spn/pkg/errors"
 	"github.com/tendermint/spn/x/profile/types"
 )
 
@@ -21,42 +20,38 @@ func (k msgServer) SetValidatorConsAddress(goCtx context.Context, msg *types.Msg
 
 	// check signature
 	currentNonce := uint64(0)
-	// consensusNonce, found := k.GetConsensusKeyNonce(ctx, msg.ConsAddress)
-	// if found {
-	// 	currentNonce = consensusNonce.Nonce
-	// }
+	consensusNonce, found := k.GetConsensusKeyNonce(ctx, msg.ConsAddress)
+	if found {
+		currentNonce = consensusNonce.Nonce
+	}
 
 	if !checkValidatorSignature(msg.Signature, msg.ConsAddress, currentNonce) {
 		return &types.MsgSetValidatorConsAddressResponse{},
 			sdkerrors.Wrapf(types.ErrInvalidValidatorSignature, "consensus address: %s / signature: %s", msg.ConsAddress, msg.Signature)
 	}
 
-	// k.SetConsensusKeyNonce(ctx, types.ConsensusKeyNonce{
-	//	ConsensusAddress: consensusNonce.ConsensusAddress,
-	//	Nonce: currentNonce+1},
-	// })
-
-	// get the current validator to eventually overwrite description
-	validator, found := k.GetValidator(ctx, validatorByConsAddr.ValidatorAddress)
-	if !found {
-		return &types.MsgSetValidatorConsAddressResponse{},
-			spnerrors.Criticalf("a validator consensus address %s is associated to a non-existent validator address %s",
-				validatorByConsAddr.ConsensusAddress,
-				validatorByConsAddr.ValidatorAddress,
-			)
+	validator := types.Validator{
+		Address:          msg.Address,
+		ConsensusAddress: msg.ConsAddress,
+		Description:      types.ValidatorDescription{},
 	}
 
-	//validator := load(ValidatorsByAddress, msg.Address)
-	//
-	//newValidator := newValidator()
-	//newValidator.Address = msg.Address
-	//newValidator.ConsAddress = msg.ConsAddress
-	//if validator != nil
-	//	newValidator.Description = validator.Description
-	//
-	//// store validator information
-	//store(ValidatorsByAddress, msg.Address, validator)
-	//store(ValidatorsByConsKey, msg.ConsAddress, msg.Address)
+	// get the current validator to eventually overwrite description
+	validatorStore, found := k.GetValidator(ctx, validatorByConsAddr.ValidatorAddress)
+	if found {
+		validator.Description = validatorStore.Description
+	}
+
+	// store validator information
+	k.SetValidator(ctx, validator)
+	k.SetValidatorByConsAddress(ctx, types.ValidatorByConsAddress{
+		ConsensusAddress: msg.ConsAddress,
+		ValidatorAddress: msg.Address,
+	})
+	k.SetConsensusKeyNonce(ctx, types.ConsensusKeyNonce{
+		ConsensusAddress: consensusNonce.ConsensusAddress,
+		Nonce:            currentNonce + 1,
+	})
 
 	return &types.MsgSetValidatorConsAddressResponse{}, nil
 }
