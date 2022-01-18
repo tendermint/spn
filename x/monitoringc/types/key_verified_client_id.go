49115a3e0003274ceebd7a5c1104808067b67b8f
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2022-01-18 17:04:40 +0100

    feat(`monitoringc`): initialize `MsgCreateClient` (#443)
    * initialize message
    
    * launch ID for message
    
    * Add ibc client keeper
    
    * Consensus client state proto
    
    * Add verified client ID map
    
    * Add query for verified client IDs
    
    * add consensus and client state to cli
    
    * Fix query
    
    * Client state generation onchain
    
    * Initialize IBC type package
    
    * consensus state type
    
    * consensus state file parse
    
    * Add chain ID to client state
    
    * validator set utility methods
    
    * lint
    
    * tests for validator set type
    
    * add validator set field
    
    * Move ibctypes to proto
    
    * refactor ibctypes
    
    * change message format
    
    * monitoring sample package
    
    * Message tests
    
    * lint
    
    * comments
    
    * TDT testing
    
    * Update proto/monitoringc/verified_client_id.proto
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * other comments
    
    * Update x/monitoringc/keeper/verified_client_id.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * Update x/monitoringc/keeper/verified_client_id.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * Update x/monitoringc/keeper/verified_client_id_test.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * Update x/monitoringc/keeper/verified_client_id_test.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * create file type
    
    * fix
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>

---
+++ x/monitoringc/types/key_verified_client_id.go
@@ -0,0 +1,25 @@
+package types
+
+import "encoding/binary"
+
+const (
+	// VerifiedClientIDKeyPrefix is the prefix to retrieve all VerifiedClientID
+	VerifiedClientIDKeyPrefix = "VerifiedClientID/value/"
+)
+
+// VerifiedClientIDsFomLaunchIDKey returns the store key to retrieve client ids from a launch ID
+func VerifiedClientIDsFomLaunchIDKey(launchID uint64) []byte {
+	launchIDBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(launchIDBytes, launchID)
+	return append(launchIDBytes, []byte("/")...)
+}
+
+// VerifiedClientIDKey returns the store key to retrieve a VerifiedClientID from the index fields
+func VerifiedClientIDKey(launchID uint64, clientID string) []byte {
+	return append(VerifiedClientIDsFomLaunchIDKey(launchID), []byte(clientID+"/")...)
+}
+
+// VerifiedClientIDsPrefix returns the prefix path to retrieve client ids from a launch ID
+func VerifiedClientIDsPrefix(launchID uint64) []byte {
+	return append([]byte(VerifiedClientIDKeyPrefix), VerifiedClientIDsFomLaunchIDKey(launchID)...)
+}
