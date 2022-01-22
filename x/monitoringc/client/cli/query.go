076aa74a925fa62e401dea5656be9d0c7718e017
ltacker <lucas.bertrand.22@gmail.com>
2022-01-22 17:46:29 +0100

    params query position

--- x/monitoringc/client/cli/query.go
+++ x/monitoringc/client/cli/query.go
@@ -25,12 +25,12 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(
-		CmdQueryParams(),
 		CmdVerifiedClientIds(),
 		CmdListProviderClientID(),
 		CmdShowProviderClientID(),
 		CmdListLaunchIDFromVerifiedClientID(),
 		CmdShowLaunchIDFromVerifiedClientID(),
+		CmdQueryParams(),
 	)
 	// this line is used by starport scaffolding # 1
 
