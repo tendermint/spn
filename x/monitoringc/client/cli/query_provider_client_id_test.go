0ecb5ef59782affdd05530d7262f335de1e6ef36
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2022-01-22 17:39:39 +0100

    Update x/monitoringc/client/cli/query_provider_client_id_test.go
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>

--- x/monitoringc/client/cli/query_provider_client_id_test.go
+++ x/monitoringc/client/cli/query_provider_client_id_test.go
@@ -18,8 +18,6 @@ import (
 	"github.com/tendermint/spn/x/monitoringc/types"
 )
 
-// Prevent strconv unused error
-var _ = strconv.IntSize
 
 func networkWithProviderClientIDObjects(t *testing.T, n int) (*network.Network, []types.ProviderClientID) {
 	t.Helper()
