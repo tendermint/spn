e51200a209d7ba292bc6aeda606a536e0a200854
ltacker <lucas.bertrand.22@gmail.com>
2022-01-31 07:35:55 +0100

    initialize packet for monitoring provider

---
+++ x/monitoringp/keeper/monitoring.go
@@ -0,0 +1,132 @@
+package keeper
+
+import (
+	"errors"
+	spntypes "github.com/tendermint/spn/pkg/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	clienttypes "github.com/cosmos/ibc-go/v2/modules/core/02-client/types"
+	channeltypes "github.com/cosmos/ibc-go/v2/modules/core/04-channel/types"
+	host "github.com/cosmos/ibc-go/v2/modules/core/24-host"
+	"github.com/tendermint/spn/x/monitoringp/types"
+)
+
+// TransmitMonitoringPacket transmits the packet over IBC with the specified source port and source channel
+func (k Keeper) TransmitMonitoringPacket(
+	ctx sdk.Context,
+	packetData spntypes.MonitoringPacket,
+	sourcePort,
+	sourceChannel string,
+	timeoutHeight clienttypes.Height,
+	timeoutTimestamp uint64,
+) error {
+
+	sourceChannelEnd, found := k.channelKeeper.GetChannel(ctx, sourcePort, sourceChannel)
+	if !found {
+		return sdkerrors.Wrapf(channeltypes.ErrChannelNotFound, "port ID (%s) channel ID (%s)", sourcePort, sourceChannel)
+	}
+
+	destinationPort := sourceChannelEnd.GetCounterparty().GetPortID()
+	destinationChannel := sourceChannelEnd.GetCounterparty().GetChannelID()
+
+	// get the next sequence
+	sequence, found := k.channelKeeper.GetNextSequenceSend(ctx, sourcePort, sourceChannel)
+	if !found {
+		return sdkerrors.Wrapf(
+			channeltypes.ErrSequenceSendNotFound,
+			"source port: %s, source channel: %s", sourcePort, sourceChannel,
+		)
+	}
+
+	channelCap, ok := k.scopedKeeper.GetCapability(ctx, host.ChannelCapabilityPath(sourcePort, sourceChannel))
+	if !ok {
+		return sdkerrors.Wrap(channeltypes.ErrChannelCapabilityNotFound, "module does not own channel capability")
+	}
+
+	// encode the packet
+	var modulePacket types.MonitoringpPacketData
+	modulePacket.Packet = &types.MonitoringpPacketData_MonitoringPacket{
+		MonitoringPacket: &packetData,
+	}
+	packetBytes, err := modulePacket.Marshal()
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, "cannot marshal the packet: "+err.Error())
+	}
+
+	packet := channeltypes.NewPacket(
+		packetBytes,
+		sequence,
+		sourcePort,
+		sourceChannel,
+		destinationPort,
+		destinationChannel,
+		timeoutHeight,
+		timeoutTimestamp,
+	)
+
+	if err := k.channelKeeper.SendPacket(ctx, channelCap, packet); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// OnRecvMonitoringPacket processes packet reception
+func (k Keeper) OnRecvMonitoringPacket(
+	ctx sdk.Context,
+	packet channeltypes.Packet,
+	data spntypes.MonitoringPacket,
+	) (packetAck types.MonitoringPacketAck, err error) {
+	// validate packet data upon receiving
+	if err := data.ValidateBasic(); err != nil {
+		return packetAck, err
+	}
+
+	// TODO: packet reception logic
+
+	return packetAck, nil
+}
+
+// OnAcknowledgementMonitoringPacket responds to the the success or failure of a packet
+// acknowledgement written on the receiving chain.
+func (k Keeper) OnAcknowledgementMonitoringPacket(
+	ctx sdk.Context,
+	packet channeltypes.Packet,
+	data  spntypes.MonitoringPacket,
+	ack channeltypes.Acknowledgement,
+	) error {
+	switch dispatchedAck := ack.Response.(type) {
+	case *channeltypes.Acknowledgement_Error:
+
+		// TODO: failed acknowledgement logic
+		_ = dispatchedAck.Error
+
+		return nil
+	case *channeltypes.Acknowledgement_Result:
+		// Decode the packet acknowledgment
+		var packetAck types.MonitoringPacketAck
+
+		if err := types.ModuleCdc.UnmarshalJSON(dispatchedAck.Result, &packetAck); err != nil {
+			// The counter-party module doesn't implement the correct acknowledgment format
+			return errors.New("cannot unmarshal acknowledgment")
+		}
+
+		// TODO: successful acknowledgement logic
+
+		return nil
+	default:
+		// The counter-party module doesn't implement the correct acknowledgment format
+		return errors.New("invalid acknowledgment format")
+	}
+}
+
+// OnTimeoutMonitoringPacket responds to the case where a packet has not been transmitted because of a timeout
+func (k Keeper) OnTimeoutMonitoringPacket(
+	ctx sdk.Context,
+	packet channeltypes.Packet,
+	data  spntypes.MonitoringPacket,
+	) error {
+
+	return nil
+}
