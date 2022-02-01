2464b6474c82f53822ff19c90301d1c713bfaefe
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2022-02-01 17:48:38 +0100

    feat: initialize monitoring packet handlers (#479)
    * monitoring packet validate basics
    
    * initialize packet for monitoring provider
    
    * initialize packet monitoring consumer
    
    * remove unused handlers
    
    * lint
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>

---
+++ pkg/types/monitoring_packet.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+func (m MonitoringPacket) ValidateBasic() error {
+	if err := m.SignatureCounts.Validate(); err != nil {
+		return errors.Wrap(err, "invalid signature counts")
+	}
+
+	if m.BlockHeight < m.SignatureCounts.BlockCount {
+		return fmt.Errorf(
+			"block height %d must be greater or equal to block count %d",
+			m.BlockHeight,
+			m.SignatureCounts.BlockCount,
+		)
+	}
+	return nil
+}
