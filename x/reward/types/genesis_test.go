344858cc71920f8296804b4266c5c9c26bf111df
Danilo Pantani <danpantani@gmail.com>
2022-02-01 14:37:12 -0300

    Scaffold the RewardPool map

--- x/reward/types/genesis_test.go
+++ x/reward/types/genesis_test.go
@@ -19,12 +19,34 @@ func TestGenesisState_Validate(t *testing.T) {
 			valid:    true,
 		},
 		{
-			desc:     "valid genesis state",
+			desc: "valid genesis state",
 			genState: &types.GenesisState{
 
+				RewardPoolList: []types.RewardPool{
+					{
+						LaunchID: 0,
+					},
+					{
+						LaunchID: 1,
+					},
+				},
 				// this line is used by starport scaffolding # types/genesis/validField
 			},
 			valid: true,
+		},
+		{
+			desc: "duplicated rewardPool",
+			genState: &types.GenesisState{
+				RewardPoolList: []types.RewardPool{
+					{
+						LaunchID: 0,
+					},
+					{
+						LaunchID: 0,
+					},
+				},
+			},
+			valid: false,
 		},
 		// this line is used by starport scaffolding # types/genesis/testcase
 	} {
