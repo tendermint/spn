418741e2a7818b7b3a99c43bda4103fcf33fefcf
ltacker <lucas.bertrand.22@gmail.com>
2022-02-16 13:42:04 +0100

    localnet initialization

---
+++ localnet/readme.md
@@ -0,0 +1,33 @@
+## Localnet
+
+Localnet is a simple local testnet for Starport Network that includes 3 validators.
+
+The three nodes use the following config:
+
+```
+rpc:        :26657
+p2p:        :26656
+prof:       :6060
+grpc:       :9090
+grpc-web:   :9091
+api:        :1317
+```
+*Node 1 (Joe)*
+```
+rpc:        :26655
+p2p:        :26654
+prof:       :6059
+grpc:       :9088
+grpc-web:   :9089
+api:        :1316
+```
+*Node 2 (Steve)*
+```
+rpc:        :26653
+p2p:        :26652
+prof:       :6058
+grpc:       :9086
+grpc-web:   :9087
+api:        :1315
+```
+*Node 3 (Olivia)*
---
+++ localnet/start.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import json
+import subprocess
+
+# Consumer debug mode
+debugMode = False
+
+# Staking
+# Must be lower than 200000000stake
+maxValidator = 10
+selfDelegationVal1 = '70000000stake'
+selfDelegationVal2 = '60000000stake'
+selfDelegationVal3 = '50000000stake'
+
+# Reset all nodes
+os.system('spnd unsafe-reset-all --home ./node1')
+os.system('spnd unsafe-reset-all --home ./node2')
+os.system('spnd unsafe-reset-all --home ./node3')
+
+# Open the genesis template
+genesisFile = open('./genesis_template.json')
+genesis = json.load(genesisFile)
+
+# Set timestamp
+genesis['genesis_time'] = "2022-02-10T10:29:59.410196Z"
+
+# Set monitoring module param
+genesis['app_state']['monitoringp']['params']['debugMode'] = debugMode
+# Set staking max validators
+genesis['app_state']['staking']['params']['max_validators'] = maxValidator
+
+# Create the gentxs
+os.system('spnd gentx alice {} --chain-id spn-1 --moniker="bob" --home ./node1 --output-document ./gentx1.json'.format(selfDelegationVal1))
+gentx1File = open('./gentx1.json')
+gentx1 = json.load(gentx1File)
+
+os.system('spnd gentx bob {} --chain-id spn-1 --moniker="carol" --home ./node2 --output-document ./gentx2.json'.format(selfDelegationVal2))
+gentx2File = open('./gentx2.json')
+gentx2 = json.load(gentx2File)
+
+os.system('spnd gentx carol {} --chain-id spn-1 --moniker="dave" --home ./node3 --output-document ./gentx3.json'.format(selfDelegationVal3))
+gentx3File = open('./gentx3.json')
+gentx3 = json.load(gentx3File)
+
+# Collect gentxs
+genesis['app_state']['genutil']['gen_txs'].append(gentx1)
+genesis['app_state']['genutil']['gen_txs'].append(gentx2)
+genesis['app_state']['genutil']['gen_txs'].append(gentx3)
+
+os.remove('./gentx1.json')
+os.remove('./gentx2.json')
+os.remove('./gentx3.json')
+
+# Save genesis
+with open('./node1/config/genesis.json', 'w', encoding='utf-8') as f:
+    json.dump(genesis, f, ensure_ascii=False, indent=4)
+with open('./node2/config/genesis.json', 'w', encoding='utf-8') as f:
+    json.dump(genesis, f, ensure_ascii=False, indent=4)
+with open('./node3/config/genesis.json', 'w', encoding='utf-8') as f:
+    json.dump(genesis, f, ensure_ascii=False, indent=4)
+
+print('Starting the network')
+subprocess.Popen(["spnd", "start", "--home", "./node2"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.Popen(["spnd", "start", "--home", "./node3"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.run(["spnd start --home ./node1"], shell=True, check=True)
+
