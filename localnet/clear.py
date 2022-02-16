df9d7230bf8b194c7d65466e2f981efbba40c19e
ltacker <lucas.bertrand.22@gmail.com>
2022-02-16 13:47:48 +0100

    clear network

--- localnet/clear.py
+++ localnet/clear.py
@@ -1,4 +1,4 @@
 import os
-os.system('spnd unsafe-reset-all --home ./node1')
-os.system('spnd unsafe-reset-all --home ./node2')
-os.system('spnd unsafe-reset-all --home ./node3')
+os.system('spnd unsafe-reset-all --home ./localnet/node1')
+os.system('spnd unsafe-reset-all --home ./localnet/node2')
+os.system('spnd unsafe-reset-all --home ./localnet/node3')
