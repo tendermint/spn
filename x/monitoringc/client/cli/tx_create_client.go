bfd1c05d12599c572e33305ca9521a4931f54ffb
ltacker <lucas.bertrand.22@gmail.com>
2022-02-22 14:26:46 +0100

    new tx

--- x/monitoringc/client/cli/tx_create_client.go
+++ x/monitoringc/client/cli/tx_create_client.go
@@ -11,12 +11,22 @@ import (
 	"github.com/tendermint/spn/x/monitoringc/types"
 )
 
+const (
+	flagUnbondingPeriod = "unbonding-period"
+	flagRevisionHeight = "revision-height"
+)
+
 func CmdCreateClient() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "create-client [launch-id] [consensus-state-file] [validator-set-file]",
 		Short: "Create a verified client ID to connect to the chain with the specified launch ID",
 		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			var (
+				unbondingTime, _ = cmd.Flags().GetInt64(flagUnbondingPeriod)
+				revisionHeight, _ = cmd.Flags().GetUint64(flagRevisionHeight)
+			)
+
 			launchID, err := cast.ToUint64E(args[0])
 			if err != nil {
 				return err
@@ -37,11 +47,15 @@ func CmdCreateClient() *cobra.Command {
 				return err
 			}
 
+
+
 			msg := types.NewMsgCreateClient(
 				clientCtx.GetFromAddress().String(),
 				launchID,
 				cs,
 				vs,
+				unbondingTime,
+				revisionHeight,
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
@@ -50,6 +64,8 @@ func CmdCreateClient() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().Int64(flagUnbondingPeriod, spntypes.DefaultUnbondingTime, "Custom unbonding period of the provider chain (default: 21 days)")
+	cmd.Flags().Uint64(flagRevisionHeight, 1, "Custom revision height for the IBC client of the provider chain (default: 1)")
 	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
