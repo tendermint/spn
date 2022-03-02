ec2f6f166f052459c4198fdc742ba2ab2263f2e3
aljo242 <alex.johnson@tendermint.com>
2022-03-02 18:57:59 -0500

    fmt

--- x/reward/types/message_set_reward.go
+++ x/reward/types/message_set_reward.go
@@ -46,5 +46,10 @@ func (msg *MsgSetRewards) ValidateBasic() error {
 	if err := msg.Coins.Validate(); err != nil {
 		return sdkerrors.Wrapf(ErrInvalidRewardPoolCoins, "invalid reward pool coins (%s)", err)
 	}
+
+	if msg.LastRewardHeight < 0 {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "last reward height must be non-negative")
+	}
+
 	return nil
 }
