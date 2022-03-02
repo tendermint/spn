df18f91b175e8243b3f793c499c2be9bb618d734
ltacker <lucas.bertrand.22@gmail.com>
2022-03-02 15:41:10 +0100

    new codec

--- x/profile/types/codec.go
+++ x/profile/types/codec.go
@@ -12,7 +12,7 @@ import (
 func RegisterCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgUpdateValidatorDescription{}, "profile/UpdateValidatorDescription", nil)
 	cdc.RegisterConcrete(&MsgDeleteValidator{}, "profile/DeleteValidator", nil)
-	cdc.RegisterConcrete(&MsgSetValidatorConsAddress{}, "profile/SetValidatorConsAddress", nil)
+	cdc.RegisterConcrete(&MsgAddValidatorOperatorAddress{}, "profile/AddValidatorOperatorAddress", nil)
 	cdc.RegisterConcrete(&MsgCreateCoordinator{}, "profile/CreateCoordinator", nil)
 	cdc.RegisterConcrete(&MsgUpdateCoordinatorDescription{}, "profile/UpdateCoordinatorDescription", nil)
 	cdc.RegisterConcrete(&MsgUpdateCoordinatorAddress{}, "profile/UpdateCoordinatorAddress", nil)
@@ -24,7 +24,8 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgUpdateValidatorDescription{},
 		&MsgDeleteValidator{},
-		&MsgSetValidatorConsAddress{},
+		&MsgAddValidatorOperatorAddress{},
+		&MsgAddValidatorOperatorAddress{},
 		&MsgCreateCoordinator{},
 		&MsgUpdateCoordinatorDescription{},
 		&MsgUpdateCoordinatorAddress{},
