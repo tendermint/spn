d08169c9ee3f7c43c907b0b1ab4474e009ecc1dc
ltacker <lucas.bertrand.22@gmail.com>
2022-03-03 13:35:34 +0100

    remove message

--- x/profile/types/errors.go
+++ x/profile/types/errors.go
@@ -12,7 +12,6 @@ var (
 	ErrCoordAddressNotFound      = sdkerrors.Register(ModuleName, 3, "coordinator address not found")
 	ErrCoordInvalid              = sdkerrors.Register(ModuleName, 4, "invalid coordinator")
 	ErrEmptyDescription          = sdkerrors.Register(ModuleName, 5, "you must provide at least one description parameter")
-	ErrValidatorNotFound         = sdkerrors.Register(ModuleName, 6, "validator address not found")
 	ErrInvalidValidatorSignature = sdkerrors.Register(ModuleName, 7, "invalid validator signature")
 	ErrInvalidValidatorKey       = sdkerrors.Register(ModuleName, 8, "invalid validator key")
 	ErrInvalidValidatorNonce     = sdkerrors.Register(ModuleName, 9, "invalid validator nonce")
