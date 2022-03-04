8d307e99867c5e4875256ff402b0519efe3e2fbd
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2022-03-04 16:16:48 +0100

    feat(`participation`): initialize module (#594)
    * init participation module
    
    * address feedback
    
    * add params in localnet genesis template for participation
    
    * improve participation CLI cmd description
    
    * small edit for f/b
    
    * fix test error
    
    Co-authored-by: Alex Johnson <alex.johnson@tendermint.com>

--- app/app.go
+++ app/app.go
@@ -115,6 +115,10 @@ import (
 	rewardmodulekeeper "github.com/tendermint/spn/x/reward/keeper"
 	rewardmoduletypes "github.com/tendermint/spn/x/reward/types"
 
+	participationmodule "github.com/tendermint/spn/x/participation"
+	participationmodulekeeper "github.com/tendermint/spn/x/participation/keeper"
+	participationmoduletypes "github.com/tendermint/spn/x/participation/types"
+
 	// this line is used by starport scaffolding # stargate/app/moduleImport
 
 	"github.com/tendermint/starport/starport/pkg/cosmoscmd"
@@ -169,6 +173,7 @@ var (
 		evidence.AppModuleBasic{},
 		transfer.AppModuleBasic{},
 		vesting.AppModuleBasic{},
+		participationmodule.AppModuleBasic{},
 		// this line is used by starport scaffolding # stargate/app/moduleBasic
 		profilemodule.AppModuleBasic{},
 		launchmodule.AppModuleBasic{},
@@ -251,11 +256,12 @@ type App struct {
 
 	ScopedMonitoringcKeeper capabilitykeeper.ScopedKeeper
 
-	ProfileKeeper     profilemodulekeeper.Keeper
-	LaunchKeeper      launchmodulekeeper.Keeper
-	CampaignKeeper    campaignmodulekeeper.Keeper
-	MonitoringcKeeper monitoringcmodulekeeper.Keeper
-	RewardKeeper      rewardmodulekeeper.Keeper
+	ProfileKeeper       profilemodulekeeper.Keeper
+	LaunchKeeper        launchmodulekeeper.Keeper
+	CampaignKeeper      campaignmodulekeeper.Keeper
+	MonitoringcKeeper   monitoringcmodulekeeper.Keeper
+	RewardKeeper        rewardmodulekeeper.Keeper
+	ParticipationKeeper participationmodulekeeper.Keeper
 	// this line is used by starport scaffolding # stargate/app/keeperDeclaration
 
 	// the module manager
@@ -310,6 +316,7 @@ func New(
 		monitoringcmoduletypes.StoreKey,
 		rewardmoduletypes.StoreKey,
 		fundraisingtypes.StoreKey,
+		participationmoduletypes.StoreKey,
 		// this line is used by starport scaffolding # stargate/app/storeKey
 	)
 	tkeys := sdk.NewTransientStoreKeys(paramstypes.TStoreKey)
@@ -478,6 +485,16 @@ func New(
 	)
 	monitoringcModule := monitoringcmodule.NewAppModule(appCodec, app.MonitoringcKeeper, app.AuthKeeper, app.BankKeeper)
 
+	app.ParticipationKeeper = *participationmodulekeeper.NewKeeper(
+		appCodec,
+		keys[participationmoduletypes.StoreKey],
+		keys[participationmoduletypes.MemStoreKey],
+		app.GetSubspace(participationmoduletypes.ModuleName),
+		app.FundraisingKeeper,
+		app.StakingKeeper,
+	)
+	participationModule := participationmodule.NewAppModule(appCodec, app.ParticipationKeeper)
+
 	// this line is used by starport scaffolding # stargate/app/keeperDefinition
 
 	// Create static IBC router, add transfer route, then set and seal it
@@ -524,6 +541,7 @@ func New(
 		monitoringcModule,
 		rewardModule,
 		fundraisingmodule.NewAppModule(appCodec, app.FundraisingKeeper, app.AuthKeeper, app.BankKeeper),
+		participationModule,
 		// this line is used by starport scaffolding # stargate/app/appModule
 	)
 
@@ -579,6 +597,7 @@ func New(
 		monitoringcmoduletypes.ModuleName,
 		rewardmoduletypes.ModuleName,
 		fundraisingtypes.ModuleName,
+		participationmoduletypes.ModuleName,
 		// this line is used by starport scaffolding # stargate/app/initGenesis
 	)
 
@@ -801,6 +820,7 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	paramsKeeper.Subspace(monitoringcmoduletypes.ModuleName)
 	paramsKeeper.Subspace(rewardmoduletypes.ModuleName)
 	paramsKeeper.Subspace(fundraisingtypes.ModuleName)
+	paramsKeeper.Subspace(participationmoduletypes.ModuleName)
 	// this line is used by starport scaffolding # stargate/app/paramSubspace
 
 	return paramsKeeper
---
+++ x/participation/keeper/params.go
@@ -0,0 +1,39 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/tendermint/spn/x/participation/types"
+)
+
+// GetParams get all parameters as types.Params
+func (k Keeper) GetParams(ctx sdk.Context) types.Params {
+	return types.NewParams(
+		k.AllocationPrice(ctx),
+		k.ParticipationTierList(ctx),
+		k.WithdrawalAllocationDelay(ctx),
+	)
+}
+
+// SetParams set the params
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramstore.SetParamSet(ctx, &params)
+}
+
+// AllocationPrice returns the AllocationPrice param
+func (k Keeper) AllocationPrice(ctx sdk.Context) (res types.AllocationPrice) {
+	k.paramstore.Get(ctx, types.KeyAllocationPrice, &res)
+	return
+}
+
+// ParticipationTierList returns the ParticipationTierList param
+func (k Keeper) ParticipationTierList(ctx sdk.Context) (res []types.Tier) {
+	k.paramstore.Get(ctx, types.KeyParticipationTierList, &res)
+	return
+}
+
+// WithdrawalAllocationDelay returns the WithdrawalAllocationDelay param
+func (k Keeper) WithdrawalAllocationDelay(ctx sdk.Context) (res uint64) {
+	k.paramstore.Get(ctx, types.KeyWithdrawalAllocationDelay, &res)
+	return
+}
