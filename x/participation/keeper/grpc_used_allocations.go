9e882ac0fd6f4c91d937a1c7c7708adc9f90b9d9
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2022-03-07 21:15:54 +0100

    fmt pass

--- x/participation/keeper/grpc_used_allocations.go
+++ x/participation/keeper/grpc_used_allocations.go
@@ -6,9 +6,10 @@ import (
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
-	"github.com/tendermint/spn/x/participation/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"github.com/tendermint/spn/x/participation/types"
 )
 
 func (k Keeper) UsedAllocationsAll(c context.Context, req *types.QueryAllUsedAllocationsRequest) (*types.QueryAllUsedAllocationsResponse, error) {
