4dbbd75ef852d2dccad08d51b16ed3f6ebc52b13
ltacker <lucas.bertrand.22@gmail.com>
2022-03-14 16:18:30 +0100

    use mint

--- testutil/keeper/bank.go
+++ testutil/keeper/bank.go
@@ -2,15 +2,14 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	"github.com/stretchr/testify/require"
-
-	campaigntypes "github.com/tendermint/spn/x/campaign/types"
 )
 
 // Mint mints the specified coins into the account balance
 func (tk TestKeepers) Mint(ctx sdk.Context, address string, coins sdk.Coins) {
 	sdkAddr, err := sdk.AccAddressFromBech32(address)
 	require.NoError(tk.T, err)
-	require.NoError(tk.T, tk.BankKeeper.MintCoins(ctx, campaigntypes.ModuleName, coins))
-	require.NoError(tk.T, tk.BankKeeper.SendCoinsFromModuleToAccount(ctx, campaigntypes.ModuleName, sdkAddr, coins))
+	require.NoError(tk.T, tk.BankKeeper.MintCoins(ctx, minttypes.ModuleName, coins))
+	require.NoError(tk.T, tk.BankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, sdkAddr, coins))
 }
