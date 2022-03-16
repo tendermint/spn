2bf964092ba38b282a8f90a5dced2363f9686e65
aljo242 <alex.johnson@tendermint.com>
2022-03-16 17:24:02 -0400

    format

--- x/participation/keeper/grpc_available_allocations.go
+++ x/participation/keeper/grpc_available_allocations.go
@@ -4,9 +4,10 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/tendermint/spn/x/participation/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"github.com/tendermint/spn/x/participation/types"
 )
 
 func (k Keeper) AvailableAllocations(goCtx context.Context,
