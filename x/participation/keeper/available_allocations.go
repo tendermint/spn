89827ef3a56210e1abec8ee3f18bd538fdcabbea
aljo242 <alex.johnson@tendermint.com>
2022-03-16 15:59:53 -0400

    add available allocations keeper method

--- x/participation/keeper/available_allocations.go
+++ x/participation/keeper/available_allocations.go
@@ -6,10 +6,17 @@ import (
 
 // GetAvailableAllocation returns the number of allocations that are unused
 func (k Keeper) GetAvailableAllocations(ctx sdk.Context, address string) (uint64, error) {
-	_, err := k.GetTotalAllocations(ctx, address)
+	numTotalAlloc, err := k.GetTotalAllocations(ctx, address)
 	if err != nil {
 		return 0, err
 	}
 
-	return 0, nil
+	usedAlloc, found := k.GetUsedAllocations(ctx, address)
+	if !found {
+		return numTotalAlloc, nil
+	}
+
+	availableAlloc := numTotalAlloc - usedAlloc.NumAllocations
+
+	return availableAlloc, nil
 }
--- x/participation/keeper/available_allocations_test.go
+++ x/participation/keeper/available_allocations_test.go
@@ -1,1 +1,76 @@
 package keeper_test
+
+import (
+	"strconv"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	testkeeper "github.com/tendermint/spn/testutil/keeper"
+	"github.com/tendermint/spn/testutil/sample"
+	"github.com/tendermint/spn/x/participation/types"
+)
+
+func TestAvailableAllocationsGet(t *testing.T) {
+	sdkCtx, tk, _ := testkeeper.NewTestSetup(t)
+
+	invalidAddress := strconv.Itoa(1)
+	allocationPrice := types.AllocationPrice{Bonded: sdk.NewInt(100)}
+
+	tk.ParticipationKeeper.SetParams(sdkCtx, types.Params{
+		AllocationPrice: allocationPrice,
+	})
+
+	validAddress := sample.Address()
+	validAddressNoUse := sample.Address()
+	addressNegativeDelegations := sample.Address()
+
+	tk.DelegateN(sdkCtx, validAddress, 100, 10)
+	tk.DelegateN(sdkCtx, validAddressNoUse, 100, 10)
+	tk.DelegateN(sdkCtx, addressNegativeDelegations, -100, 10)
+
+	tk.ParticipationKeeper.SetUsedAllocations(sdkCtx, types.UsedAllocations{
+		Address:        validAddress,
+		NumAllocations: 2,
+	})
+
+	for _, tc := range []struct {
+		desc       string
+		address    string
+		allocation uint64
+		wantError  bool
+	}{
+		{
+			desc:       "valid address with used allocations",
+			address:    validAddress,
+			allocation: 8, // (100 * 10 / 100) - 2 = 8
+		},
+		{
+			desc:       "valid address with no used allocations",
+			address:    validAddressNoUse,
+			allocation: 10, // (100 * 10 / 100) - 0 = 10
+		},
+		{
+			desc:      "invalid address returns error",
+			address:   invalidAddress,
+			wantError: true,
+		},
+		{
+			desc:      "negative delegations will yield invalid allocation",
+			address:   addressNegativeDelegations,
+			wantError: true,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			alloc, err := tk.ParticipationKeeper.GetAvailableAllocations(sdkCtx, tc.address)
+			if tc.wantError {
+				require.Error(t, err)
+				return
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.allocation, alloc)
+			}
+		})
+	}
+}
