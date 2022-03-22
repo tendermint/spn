803ef8a81c84921a0be0e981725852541ca886d8
aljo242 <alex.johnson@tendermint.com>
2022-03-22 13:13:30 -0400

    refactor into simulation.go

--- x/participation/simulation/simulation.go
+++ x/participation/simulation/simulation.go
@@ -34,7 +34,7 @@ func SimulateMsgParticipate(
 func SimulateMsgWithdrawAllocations(
 	_ authkeeper.AccountKeeper,
 	_ bankkeeper.Keeper,
-	k keeper.Keeper,
+	_ keeper.Keeper,
 ) simtypes.Operation {
 	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
