56be2c1fea4940d175b0b6539034d99a3cd94a2a
aljo242 <alex.johnson@tendermint.com>
2022-04-11 17:34:22 -0400

    refactor based on comments

--- x/reward/simulation/simulation.go
+++ x/reward/simulation/simulation.go
@@ -16,6 +16,10 @@ import (
 const (
 	ProbabilityCreateRewardPool = 70
 	ProbabilityCloseRewardPool  = 70
+
+	ActionCreate = iota
+	ActionEdit
+	ActionClose
 )
 
 func SimulateMsgSetRewards(
@@ -26,16 +30,20 @@ func SimulateMsgSetRewards(
 	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
 		msg := &types.MsgSetRewards{}
-
 		createRewardPool := r.Int63n(100) < ProbabilityCreateRewardPool
 		closeRewardPool := r.Int63n(100) < ProbabilityCloseRewardPool
 		checkBalance := true
 
-		wantCoin := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(r.Int63n(1_000_000))))
-		if !createRewardPool && closeRewardPool { // no coins required if closing reward pool
+		// choose action to be taken
+		action := ActionCreate
+		if !createRewardPool && closeRewardPool {
+			action = ActionClose
 			checkBalance = false
+		} else if !createRewardPool && !closeRewardPool {
+			action = ActionEdit
 		}
 
+		wantCoin := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(r.Int63n(1_000_000))))
 		chain, found := FindRandomChainWithCoordBalance(r, ctx, k, bk, createRewardPool, checkBalance, wantCoin)
 		if !found {
 			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "no viable chain to be found"), nil, nil
@@ -56,23 +64,24 @@ func SimulateMsgSetRewards(
 			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "simulation account not found for chain coordinator"), nil, nil
 		}
 
+		// initialize basic message
 		msg = &types.MsgSetRewards{
 			Provider: simAccount.Address.String(),
 			LaunchID: chain.LaunchID,
 		}
 
-		if createRewardPool {
+		// set message based on action to be taken
+		switch action {
+		case ActionCreate:
 			msg.LastRewardHeight = ctx.BlockHeight() + r.Int63n(1000)
 			msg.Coins = wantCoin
-		} else {
-			if closeRewardPool {
-				msg.LastRewardHeight = 0
-				msg.Coins = sdk.NewCoins()
-			} else {
-				pool, _ := k.GetRewardPool(ctx, chain.LaunchID)
-				msg.LastRewardHeight = pool.LastRewardHeight + r.Int63n(1000)
-				msg.Coins = wantCoin
-			}
+		case ActionEdit:
+			pool, _ := k.GetRewardPool(ctx, chain.LaunchID)
+			msg.LastRewardHeight = pool.LastRewardHeight + r.Int63n(1000)
+			msg.Coins = wantCoin
+		case ActionClose:
+			msg.LastRewardHeight = 0
+			msg.Coins = sdk.NewCoins()
 		}
 
 		txCtx := simulation.OperationInput{
