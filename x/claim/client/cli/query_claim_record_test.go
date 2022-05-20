ec3403a07a144cc83d7305bf5dab4984afee6328
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2022-05-20 14:36:50 +0200

    Update x/claim/client/cli/query_claim_record_test.go
    Co-authored-by: Giuseppe Natale <12249307+giunatale@users.noreply.github.com>

--- x/claim/client/cli/query_claim_record_test.go
+++ x/claim/client/cli/query_claim_record_test.go
@@ -71,7 +71,6 @@ func TestShowClaimRecord(t *testing.T) {
 			err:  status.Error(codes.NotFound, "not found"),
 		},
 	} {
-		tc := tc
 		t.Run(tc.desc, func(t *testing.T) {
 			args := []string{
 				tc.address,
