8b50d5459a8d07c0f640d65c80fbb6c7148cd79d
ltacker <lucas.bertrand.22@gmail.com>
2022-05-20 14:59:55 +0200

    x/claim/types/claim_record_test.go

--- x/claim/types/mission_test.go
+++ x/claim/types/mission_test.go
@@ -3,24 +3,14 @@ package types_test
 import (
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 
+	tc "github.com/tendermint/spn/testutil/constructor"
 	"github.com/tendermint/spn/testutil/sample"
 	claim "github.com/tendermint/spn/x/claim/types"
 )
 
 func TestMission_Validate(t *testing.T) {
-	invalidWeightGreaterThanOne := sample.Mission(r)
-	amt, err := sdk.NewDecFromStr("1.0000001")
-	require.NoError(t, err)
-	invalidWeightGreaterThanOne.Weight = amt
-
-	invalidWeightLessThanZero := sample.Mission(r)
-	amt, err = sdk.NewDecFromStr("-0.0000001")
-	require.NoError(t, err)
-	invalidWeightLessThanZero.Weight = amt
-
 	for _, tc := range []struct {
 		desc    string
 		mission claim.Mission
@@ -32,14 +22,40 @@ func TestMission_Validate(t *testing.T) {
 			valid:   true,
 		},
 		{
-			desc:    "invalid weight - greater than 1",
-			mission: invalidWeightGreaterThanOne,
-			valid:   false,
+			desc: "should accept weigth 0",
+			mission: claim.Mission{
+				MissionID:   sample.Uint64(r),
+				Description: sample.String(r, 30),
+				Weight:      tc.Dec(t, "0"),
+			},
+			valid: true,
 		},
 		{
-			desc:    "invalid weight - less than 0",
-			mission: invalidWeightLessThanZero,
-			valid:   false,
+			desc: "should accept weight 1",
+			mission: claim.Mission{
+				MissionID:   sample.Uint64(r),
+				Description: sample.String(r, 30),
+				Weight:      tc.Dec(t, "1"),
+			},
+			valid: true,
+		},
+		{
+			desc: "should prevent weight greater than 1",
+			mission: claim.Mission{
+				MissionID:   sample.Uint64(r),
+				Description: sample.String(r, 30),
+				Weight:      tc.Dec(t, "1.0000001"),
+			},
+			valid: false,
+		},
+		{
+			desc: "should prevent weight less than 0",
+			mission: claim.Mission{
+				MissionID:   sample.Uint64(r),
+				Description: sample.String(r, 30),
+				Weight:      tc.Dec(t, "-0.0000001"),
+			},
+			valid: false,
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
