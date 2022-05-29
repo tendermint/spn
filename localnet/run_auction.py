573281ac528d03b644d5ae03e320ec0d0329a869
ltacker <lucas.bertrand.22@gmail.com>
2022-05-29 10:58:25 +0200

    fix auction script

--- localnet/run_auction.py
+++ localnet/run_auction.py
@@ -1,12 +1,13 @@
 import json
 import os
 import datetime
+import time
 from utils import cmd_devnull, cmd, initialize_campaign, date_f
 
 auction_template_file = './auctions/auction_template.json'
-auction_file = './auctions//auction.json'
+auction_file = './auctions/auction.json'
 
-def set_sale_json(selling_denom, selling_amount, paying_denom, start_price, min_bid_price, start_time, end_time):
+def set_auction_json(selling_denom, selling_amount, paying_denom, start_price, min_bid_price, start_time, end_time):
     f = open(auction_template_file)
     jf = json.load(f)
     jf['selling_coin']['denom'] = selling_denom
@@ -15,7 +16,7 @@ def set_sale_json(selling_denom, selling_amount, paying_denom, start_price, min_
     jf['start_price'] = start_price
     jf['min_bid_price'] = min_bid_price
     jf['start_time'] = start_time
-    jf['end_time'].append(end_time)
+    jf['end_time'] = end_time
     with open(auction_file, 'w', encoding='utf-8') as newF:
         json.dump(jf, newF, ensure_ascii=False, indent=4)
 
@@ -28,8 +29,8 @@ if __name__ == "__main__":
     end = date_now + datetime.timedelta(seconds=40)
 
     # Fundraising
-    set_sale_json('v/1/orbit', '50000', 'uspn', '100', '50', date_f(start), date_f(end))
-    cmd_devnull('spnd tx fundraising create-fixed-price-auction {} --from alice -y'.format(auction_file))
+    set_auction_json('v/1/orbit', '50000', 'uspn', '100', '50', date_f(start), date_f(end))
+    cmd('spnd tx fundraising create-batch-auction {} --from alice -y'.format(auction_file))
     os.remove(auction_file)
     cmd_devnull('spnd tx participation participate 1 4 --from bob -y')
     cmd_devnull('spnd tx participation participate 1 4 --from carol -y')
@@ -40,9 +41,9 @@ if __name__ == "__main__":
     time.sleep(15)
 
     # Place bid
-    cmd('spnd tx fundraising bid 1 fixed-price 100 10000v/1/orbit --from bob -y')
-    cmd('spnd tx fundraising bid 1 fixed-price 100 20000v/1/orbit --from carol -y')
-    cmd('spnd tx fundraising bid 1 fixed-price 100 20000v/1/orbit --from dave -y')
+    cmd('spnd tx fundraising bid 1 batch-many 120 10000v/1/orbit --from bob -y')
+    cmd('spnd tx fundraising bid 1 batch-many 80 20000v/1/orbit --from carol -y')
+    cmd('spnd tx fundraising bid 1 batch-many 140 20000v/1/orbit --from dave -y')
 
     # Wait withdrawal delay
     print("waiting for withdrawal delay...")
