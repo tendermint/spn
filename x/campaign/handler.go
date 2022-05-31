cbc14af86575e22a7e920c6c989f1cddbfc92031
Lucas Bertrand <lucas.bertrand.22@gmail.com>
2022-05-31 16:36:23 +0200

    refactor(`campaign`): set correct order with `MsgUpdateSpecialAllocations` (#820)
    * message order
    
    * codec
    
    Co-authored-by: Alex Johnson <alex.johnson@tendermint.com>

--- x/campaign/handler.go
+++ x/campaign/handler.go
@@ -27,6 +27,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgUpdateTotalSupply:
 			res, err := msgServer.UpdateTotalSupply(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgUpdateSpecialAllocations:
+			res, err := msgServer.UpdateSpecialAllocations(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgInitializeMainnet:
 			res, err := msgServer.InitializeMainnet(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
@@ -44,9 +47,6 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgUnredeemVouchers:
 			res, err := msgServer.UnredeemVouchers(sdk.WrapSDKContext(ctx), msg)
-			return sdk.WrapServiceResult(ctx, res, err)
-		case *types.MsgUpdateSpecialAllocations:
-			res, err := msgServer.UpdateSpecialAllocations(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 			// this line is used by starport scaffolding # 1
 
