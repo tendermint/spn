923b008db1604d9515c3aa326af180dbe1f5f0af
ltacker <lucas.bertrand.22@gmail.com>
2022-06-03 11:44:52 +0200

    launch state

--- testutil/networksuite/networksuite.go
+++ testutil/networksuite/networksuite.go
@@ -2,9 +2,10 @@
 package networksuite
 
 import (
+	"math/rand"
+
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-
 	"github.com/tendermint/spn/testutil/network"
 	"github.com/tendermint/spn/testutil/sample"
 	launch "github.com/tendermint/spn/x/launch/types"
@@ -26,6 +27,18 @@ func (nts *NetworkTestSuite) SetupSuite() {
 	launchState := launch.GenesisState{}
 	require.NoError(nts.T(), cfg.Codec.UnmarshalJSON(cfg.GenesisState[launch.ModuleName], &launchState))
 
+	launchState = populateLaunch(r, launchState)
+
+	buf, err := cfg.Codec.MarshalJSON(&launchState)
+	require.NoError(nts.T(), err)
+	cfg.GenesisState[launch.ModuleName] = buf
+
+	nts.Network = network.New(nts.T(), cfg)
+	nts.LaunchState = launchState
+}
+
+func populateLaunch(r *rand.Rand, launchState launch.GenesisState) launch.GenesisState {
+	// add chains
 	for i := 0; i < 5; i++ {
 		chain := sample.Chain(r, uint64(i), uint64(i))
 		launchState.ChainList = append(
@@ -33,10 +46,40 @@ func (nts *NetworkTestSuite) SetupSuite() {
 			chain,
 		)
 	}
-	buf, err := cfg.Codec.MarshalJSON(&launchState)
-	require.NoError(nts.T(), err)
-	cfg.GenesisState[launch.ModuleName] = buf
 
-	nts.Network = network.New(nts.T(), cfg)
-	nts.LaunchState = launchState
+	// add genesis accounts
+	for i := 0; i < 5; i++ {
+		launchState.GenesisAccountList = append(
+			launchState.GenesisAccountList,
+			sample.GenesisAccount(r, 0, sample.Address(r)),
+		)
+	}
+
+	// add vesting accounts
+	for i := 0; i < 5; i++ {
+		launchState.VestingAccountList = append(
+			launchState.VestingAccountList,
+			sample.VestingAccount(r, 0, sample.Address(r)),
+		)
+	}
+
+	// add genesis validators
+	for i := 0; i < 5; i++ {
+		launchState.GenesisValidatorList = append(
+			launchState.GenesisValidatorList,
+			sample.GenesisValidator(r, uint64(0), sample.Address(r)),
+		)
+	}
+
+	// add request
+	for i := 0; i < 5; i++ {
+		request := sample.Request(r, 0, sample.Address(r))
+		request.RequestID = uint64(i)
+		launchState.RequestList = append(
+			launchState.RequestList,
+			request,
+		)
+	}
+
+	return launchState
 }
