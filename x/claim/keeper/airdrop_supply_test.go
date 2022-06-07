a95338037675f77a4052892e643f4b4ca64382ba
ltacker <lucas.bertrand.22@gmail.com>
2022-06-07 13:22:27 +0200

    airdrop supply initial test

--- x/claim/keeper/airdrop_supply_test.go
+++ x/claim/keeper/airdrop_supply_test.go
@@ -1,30 +1,48 @@
 package keeper_test
 
 import (
+	tc "github.com/tendermint/spn/testutil/constructor"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 
-	keepertest "github.com/tendermint/spn/testutil/keeper"
+	testkeeper "github.com/tendermint/spn/testutil/keeper"
 	"github.com/tendermint/spn/testutil/nullify"
 	"github.com/tendermint/spn/testutil/sample"
-	"github.com/tendermint/spn/x/claim/keeper"
 )
 
-func createTestAirdropSupply(keeper *keeper.Keeper, ctx sdk.Context) sdk.Coin {
-	item := sample.Coin(r)
-	keeper.SetAirdropSupply(ctx, item)
-	return item
-}
-
 func TestAirdropSupplyGet(t *testing.T) {
-	k, ctx := keepertest.ClaimKeeper(t)
-	item := createTestAirdropSupply(k, ctx)
-	rst, found := k.GetAirdropSupply(ctx)
+	ctx, tk, _ := testkeeper.NewTestSetup(t)
+
+	sampleSupply := sample.Coin(r)
+	tk.ClaimKeeper.SetAirdropSupply(ctx, sampleSupply)
+
+	rst, found := tk.ClaimKeeper.GetAirdropSupply(ctx)
 	require.True(t, found)
 	require.Equal(t,
-		nullify.Fill(&item),
+		nullify.Fill(&sampleSupply),
 		nullify.Fill(&rst),
 	)
+}
+
+func TestKeeper_InitializeAirdropSupply(t *testing.T) {
+	// TODO: use mock for bank module to test critical errors
+	ctx, tk, _ := testkeeper.NewTestSetup(t)
+
+	tests := []struct {
+		name          string
+		airdropSupply sdk.Coin
+	}{
+		{
+			name:          "should allows to set airdrop supply",
+			airdropSupply: tc.Coin(t, "10000foo"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tk.ClaimKeeper.InitializeAirdropSupply(ctx, tt.airdropSupply)
+			require.NoError(t, err)
+		})
+	}
 }
