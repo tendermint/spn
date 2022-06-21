141c907ea18c903c6978d5d6040f32ee5ee39c1d
ltacker <lucas.bertrand.22@gmail.com>
2022-06-21 10:13:23 +0200

    implement message

--- x/claim/keeper/msg_claim_initial.go
+++ x/claim/keeper/msg_claim_initial.go
@@ -4,14 +4,26 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 	"github.com/tendermint/spn/x/claim/types"
 )
 
 func (k msgServer) ClaimInitial(goCtx context.Context, msg *types.MsgClaimInitial) (*types.MsgClaimInitialResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// retrieve initial claim information
+	initialClaim, found := k.GetInitialClaim(ctx)
+	if !found {
+		return nil, types.ErrInitialClaimNotFound
+	}
+	if !initialClaim.Enabled {
+		return nil, types.ErrInitialClaimNotEnabled
+	}
+
+	if err := k.CompleteMission(ctx, initialClaim.MissionID, msg.Claimer); err != nil {
+		return nil, sdkerrors.Wrap(types.ErrMissionCompleteFailure, err.Error())
+	}
 
 	return &types.MsgClaimInitialResponse{}, nil
 }
--- x/claim/types/errors.go
+++ x/claim/types/errors.go
@@ -8,8 +8,11 @@ import (
 
 // x/claim module sentinel errors
 var (
-	ErrMissionNotFound       = sdkerrors.Register(ModuleName, 2, "mission not found")
-	ErrClaimRecordNotFound   = sdkerrors.Register(ModuleName, 3, "claim record not found")
-	ErrMissionCompleted      = sdkerrors.Register(ModuleName, 4, "mission already completed")
-	ErrAirdropSupplyNotFound = sdkerrors.Register(ModuleName, 5, "airdrop supply not found")
+	ErrMissionNotFound        = sdkerrors.Register(ModuleName, 2, "mission not found")
+	ErrClaimRecordNotFound    = sdkerrors.Register(ModuleName, 3, "claim record not found")
+	ErrMissionCompleted       = sdkerrors.Register(ModuleName, 4, "mission already completed")
+	ErrAirdropSupplyNotFound  = sdkerrors.Register(ModuleName, 5, "airdrop supply not found")
+	ErrInitialClaimNotFound   = sdkerrors.Register(ModuleName, 6, "initial claim information not found")
+	ErrInitialClaimNotEnabled = sdkerrors.Register(ModuleName, 7, "initial claim not enabled")
+	ErrMissionCompleteFailure = sdkerrors.Register(ModuleName, 7, "mission failed to complete")
 )
