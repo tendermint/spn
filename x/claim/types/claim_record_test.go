8fdb12c5b993c995e1a0bc9a27932faab4155d36
ltacker <lucas.bertrand.22@gmail.com>
2022-06-21 08:24:03 +0200

    format

--- x/claim/types/claim_record_test.go
+++ x/claim/types/claim_record_test.go
@@ -1,8 +1,9 @@
 package types_test
 
 import (
-	tc "github.com/tendermint/spn/testutil/constructor"
 	"testing"
+
+	tc "github.com/tendermint/spn/testutil/constructor"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
