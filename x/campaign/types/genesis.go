6e2806a99e7ee10c6fa4435cdbba2e04509cc10c
ltacker <lucas.bertrand.22@gmail.com>
2022-07-05 17:28:50 +0200

    genesis

--- x/campaign/types/genesis.go
+++ x/campaign/types/genesis.go
@@ -59,7 +59,7 @@ func (gs GenesisState) Validate() error {
 			return fmt.Errorf("campaign id %d doesn't exist for mainnet account %s",
 				elem.CampaignID, elem.Address)
 		}
-		index := string(MainnetAccountKey(elem.CampaignID, elem.Address))
+		index := string(AccountKeyPath(elem.CampaignID, elem.Address))
 		if _, ok := mainnetAccountIndexMap[index]; ok {
 			return fmt.Errorf("duplicated index for mainnetAccount")
 		}
@@ -73,7 +73,7 @@ func (gs GenesisState) Validate() error {
 			return fmt.Errorf("campaign id %d doesn't exist for mainnet vesting account %s",
 				elem.CampaignID, elem.Address)
 		}
-		index := string(MainnetVestingAccountKey(elem.CampaignID, elem.Address))
+		index := string(AccountKeyPath(elem.CampaignID, elem.Address))
 		if _, ok := mainnetVestingAccountIndexMap[index]; ok {
 			return fmt.Errorf("duplicated index for mainnetVestingAccount")
 		}
