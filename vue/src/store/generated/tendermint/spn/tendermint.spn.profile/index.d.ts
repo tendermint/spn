1d22b30b5cd97bfcfa475e1e2634059d3377e2c3
Lucas Btd <lucas.bertrand.22@gmail.com>
2022-07-08 14:27:29 +0200

    chore: remove vue (#888)

--- vue/src/store/generated/tendermint/spn/tendermint.spn.profile/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Coordinator } from "./module/types/profile/coordinator";
-import { CoordinatorDescription } from "./module/types/profile/coordinator";
-import { CoordinatorByAddress } from "./module/types/profile/coordinator";
-import { QueryAllCoordinatorByAddressRequest } from "./module/types/profile/query";
-import { QueryAllCoordinatorByAddressResponse } from "./module/types/profile/query";
-export { Coordinator, CoordinatorDescription, CoordinatorByAddress, QueryAllCoordinatorByAddressRequest, QueryAllCoordinatorByAddressResponse };
-declare const _default;
-export default _default;
