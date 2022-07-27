a165c8e854389809d92288f495f7d6a879ff13fb
ltacker <lucas.bertrand.22@gmail.com>
2022-07-27 13:34:45 +0200

    decay info param

--- x/claim/types/params.go
+++ x/claim/types/params.go
@@ -1,11 +1,17 @@
 package types
 
 import (
+	"fmt"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"gopkg.in/yaml.v2"
+	"time"
 )
 
 var _ paramtypes.ParamSet = (*Params)(nil)
+
+var (
+	KeyDecayInformation = []byte("DecayInformation")
+)
 
 // ParamKeyTable the param key table for launch module
 func ParamKeyTable() paramtypes.KeyTable {
@@ -13,23 +19,41 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 // NewParams creates a new Params instance
-func NewParams() Params {
-	return Params{}
+func NewParams(di DecayInformation) Params {
+	return Params{
+		DecayInformation: di,
+	}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(DecayInformation{
+		Enabled:    false,
+		DecayStart: time.UnixMilli(0),
+		DecayEnd:   time.UnixMilli(0),
+	})
 }
 
 // ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{}
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(KeyDecayInformation, &p.DecayInformation, validateDecayInformation),
+	}
 }
 
 // Validate validates the set of params
 func (p Params) Validate() error {
-	return nil
+	return validateDecayInformation(p.DecayInformation)
+}
+
+// validateDecayInformation validates the DecayInformation param
+func validateDecayInformation(v interface{}) error {
+	decayInfo, ok := v.(DecayInformation)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", v)
+	}
+
+	return decayInfo.Validate()
 }
 
 // String implements the Stringer interface.
