d0c475a7745c7e6d14c309d9ba50dd8a63a8ae18
ltacker <lucas.bertrand.22@gmail.com>
2022-07-27 11:59:27 +0200

    proto

--- x/claim/types/params.pb.go
+++ x/claim/types/params.pb.go
@@ -7,15 +7,19 @@ import (
 	fmt "fmt"
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
+	github_com_gogo_protobuf_types "github.com/gogo/protobuf/types"
+	_ "google.golang.org/protobuf/types/known/durationpb"
 	io "io"
 	math "math"
 	math_bits "math/bits"
+	time "time"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
+var _ = time.Kitchen
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the proto package it is being compiled against.
@@ -25,6 +29,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // Params defines the parameters for the module.
 type Params struct {
+	DecayInformation DecayInformation `protobuf:"bytes,1,opt,name=decayInformation,proto3" json:"decayInformation"`
 }
 
 func (m *Params) Reset()      { *m = Params{} }
@@ -59,24 +64,94 @@ func (m *Params) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Params proto.InternalMessageInfo
 
+func (m *Params) GetDecayInformation() DecayInformation {
+	if m != nil {
+		return m.DecayInformation
+	}
+	return DecayInformation{}
+}
+
+// DecayInformation defines the information about decay for the airdrop
+// when claimable airdrop amount starts to decrease and when it ends
+type DecayInformation struct {
+	DecayStart time.Duration `protobuf:"bytes,1,opt,name=decayStart,proto3,stdduration" json:"decayStart"`
+	DecayEnd   time.Duration `protobuf:"bytes,2,opt,name=decayEnd,proto3,stdduration" json:"decayEnd"`
+}
+
+func (m *DecayInformation) Reset()         { *m = DecayInformation{} }
+func (m *DecayInformation) String() string { return proto.CompactTextString(m) }
+func (*DecayInformation) ProtoMessage()    {}
+func (*DecayInformation) Descriptor() ([]byte, []int) {
+	return fileDescriptor_adbfc9fc41f7a9d2, []int{1}
+}
+func (m *DecayInformation) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *DecayInformation) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_DecayInformation.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *DecayInformation) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_DecayInformation.Merge(m, src)
+}
+func (m *DecayInformation) XXX_Size() int {
+	return m.Size()
+}
+func (m *DecayInformation) XXX_DiscardUnknown() {
+	xxx_messageInfo_DecayInformation.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_DecayInformation proto.InternalMessageInfo
+
+func (m *DecayInformation) GetDecayStart() time.Duration {
+	if m != nil {
+		return m.DecayStart
+	}
+	return 0
+}
+
+func (m *DecayInformation) GetDecayEnd() time.Duration {
+	if m != nil {
+		return m.DecayEnd
+	}
+	return 0
+}
+
 func init() {
 	proto.RegisterType((*Params)(nil), "tendermint.spn.claim.Params")
+	proto.RegisterType((*DecayInformation)(nil), "tendermint.spn.claim.DecayInformation")
 }
 
 func init() { proto.RegisterFile("claim/params.proto", fileDescriptor_adbfc9fc41f7a9d2) }
 
 var fileDescriptor_adbfc9fc41f7a9d2 = []byte{
-	// 151 bytes of a gzipped FileDescriptorProto
+	// 277 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4a, 0xce, 0x49, 0xcc,
 	0xcc, 0xd5, 0x2f, 0x48, 0x2c, 0x4a, 0xcc, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12,
 	0x29, 0x49, 0xcd, 0x4b, 0x49, 0x2d, 0xca, 0xcd, 0xcc, 0x2b, 0xd1, 0x2b, 0x2e, 0xc8, 0xd3, 0x03,
-	0x2b, 0x91, 0x12, 0x49, 0xcf, 0x4f, 0xcf, 0x07, 0x2b, 0xd0, 0x07, 0xb1, 0x20, 0x6a, 0x95, 0xf8,
-	0xb8, 0xd8, 0x02, 0xc0, 0x7a, 0xad, 0x58, 0x66, 0x2c, 0x90, 0x67, 0x70, 0x72, 0x3c, 0xf1, 0x48,
-	0x8e, 0xf1, 0xc2, 0x23, 0x39, 0xc6, 0x07, 0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0xb8, 0xf0,
-	0x58, 0x8e, 0xe1, 0xc6, 0x63, 0x39, 0x86, 0x28, 0xf5, 0xf4, 0xcc, 0x92, 0x8c, 0xd2, 0x24, 0xbd,
-	0xe4, 0xfc, 0x5c, 0x7d, 0x84, 0x05, 0xfa, 0xc5, 0x05, 0x79, 0xfa, 0x15, 0xfa, 0x10, 0x57, 0x94,
-	0x54, 0x16, 0xa4, 0x16, 0x27, 0xb1, 0x81, 0x4d, 0x36, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xa9,
-	0x90, 0x93, 0xcf, 0x9b, 0x00, 0x00, 0x00,
+	0x2b, 0x91, 0x12, 0x49, 0xcf, 0x4f, 0xcf, 0x07, 0x2b, 0xd0, 0x07, 0xb1, 0x20, 0x6a, 0xa5, 0xe4,
+	0xd2, 0xf3, 0xf3, 0xd3, 0x73, 0x52, 0xf5, 0xc1, 0xbc, 0xa4, 0xd2, 0x34, 0xfd, 0x94, 0xd2, 0xa2,
+	0xc4, 0x92, 0xcc, 0xfc, 0x3c, 0x88, 0xbc, 0x52, 0x06, 0x17, 0x5b, 0x00, 0xd8, 0x6c, 0xa1, 0x08,
+	0x2e, 0x81, 0x94, 0xd4, 0xe4, 0xc4, 0x4a, 0xcf, 0xbc, 0xb4, 0xfc, 0xa2, 0x5c, 0xb0, 0x1a, 0x09,
+	0x46, 0x05, 0x46, 0x0d, 0x6e, 0x23, 0x35, 0x3d, 0x6c, 0x16, 0xea, 0xb9, 0xa0, 0xa9, 0x76, 0x62,
+	0x39, 0x71, 0x4f, 0x9e, 0x21, 0x08, 0xc3, 0x14, 0x2b, 0x96, 0x19, 0x0b, 0xe4, 0x19, 0x94, 0x66,
+	0x30, 0x72, 0x09, 0xa0, 0x6b, 0x11, 0x72, 0xe6, 0xe2, 0x02, 0x2b, 0x0f, 0x2e, 0x49, 0x2c, 0x2a,
+	0x81, 0x5a, 0x27, 0xa9, 0x07, 0x71, 0xb3, 0x1e, 0xcc, 0xcd, 0x7a, 0x2e, 0x50, 0x37, 0x3b, 0x71,
+	0x80, 0x6c, 0x98, 0x71, 0x5f, 0x9e, 0x31, 0x08, 0x49, 0x9b, 0x90, 0x3d, 0x17, 0x07, 0x98, 0xe7,
+	0x9a, 0x97, 0x22, 0xc1, 0x44, 0xbc, 0x11, 0x70, 0x4d, 0x4e, 0x8e, 0x27, 0x1e, 0xc9, 0x31, 0x5e,
+	0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c, 0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31,
+	0xdc, 0x78, 0x2c, 0xc7, 0x10, 0xa5, 0x9e, 0x9e, 0x59, 0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f,
+	0xab, 0x8f, 0x08, 0x04, 0xfd, 0xe2, 0x82, 0x3c, 0xfd, 0x0a, 0x7d, 0x48, 0xd4, 0x94, 0x54, 0x16,
+	0xa4, 0x16, 0x27, 0xb1, 0x81, 0x6d, 0x32, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x8d, 0x20, 0xef,
+	0xf8, 0xb0, 0x01, 0x00, 0x00,
 }
 
 func (m *Params) Marshal() (dAtA []byte, err error) {
@@ -99,6 +174,55 @@ func (m *Params) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	{
+		size, err := m.DecayInformation.MarshalToSizedBuffer(dAtA[:i])
+		if err != nil {
+			return 0, err
+		}
+		i -= size
+		i = encodeVarintParams(dAtA, i, uint64(size))
+	}
+	i--
+	dAtA[i] = 0xa
+	return len(dAtA) - i, nil
+}
+
+func (m *DecayInformation) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *DecayInformation) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *DecayInformation) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	n2, err2 := github_com_gogo_protobuf_types.StdDurationMarshalTo(m.DecayEnd, dAtA[i-github_com_gogo_protobuf_types.SizeOfStdDuration(m.DecayEnd):])
+	if err2 != nil {
+		return 0, err2
+	}
+	i -= n2
+	i = encodeVarintParams(dAtA, i, uint64(n2))
+	i--
+	dAtA[i] = 0x12
+	n3, err3 := github_com_gogo_protobuf_types.StdDurationMarshalTo(m.DecayStart, dAtA[i-github_com_gogo_protobuf_types.SizeOfStdDuration(m.DecayStart):])
+	if err3 != nil {
+		return 0, err3
+	}
+	i -= n3
+	i = encodeVarintParams(dAtA, i, uint64(n3))
+	i--
+	dAtA[i] = 0xa
 	return len(dAtA) - i, nil
 }
 
@@ -119,6 +243,21 @@ func (m *Params) Size() (n int) {
 	}
 	var l int
 	_ = l
+	l = m.DecayInformation.Size()
+	n += 1 + l + sovParams(uint64(l))
+	return n
+}
+
+func (m *DecayInformation) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = github_com_gogo_protobuf_types.SizeOfStdDuration(m.DecayStart)
+	n += 1 + l + sovParams(uint64(l))
+	l = github_com_gogo_protobuf_types.SizeOfStdDuration(m.DecayEnd)
+	n += 1 + l + sovParams(uint64(l))
 	return n
 }
 
@@ -157,6 +296,155 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: Params: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field DecayInformation", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if err := m.DecayInformation.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipParams(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthParams
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *DecayInformation) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowParams
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: DecayInformation: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: DecayInformation: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field DecayStart", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if err := github_com_gogo_protobuf_types.StdDurationUnmarshal(&m.DecayStart, dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field DecayEnd", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if err := github_com_gogo_protobuf_types.StdDurationUnmarshal(&m.DecayEnd, dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipParams(dAtA[iNdEx:])
