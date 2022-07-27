037e7c1372f7a661a1d5a08c89ddf5fe8a90e92b
ltacker <lucas.bertrand.22@gmail.com>
2022-07-27 15:02:18 +0200

    implement validate tests

---
+++ x/claim/types/decay_test.go
@@ -0,0 +1,61 @@
+package types_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/spn/x/claim/types"
+	"testing"
+	"time"
+)
+
+func TestDecayInformation_Validate(t *testing.T) {
+	tests := []struct {
+		name      string
+		decayInfo types.DecayInformation
+		wantErr   bool
+	}{
+		{
+			name: "should validate decay information with disabled",
+			decayInfo: types.DecayInformation{
+				Enabled:    false,
+				DecayStart: time.UnixMilli(2000),
+				DecayEnd:   time.UnixMilli(1000),
+			},
+		},
+		{
+			name: "should validate decay information with enabled and start equals to end",
+			decayInfo: types.DecayInformation{
+				Enabled:    true,
+				DecayStart: time.UnixMilli(1000),
+				DecayEnd:   time.UnixMilli(1000),
+			},
+		},
+		{
+			name: "should validate decay information with enabled and end greater than start",
+			decayInfo: types.DecayInformation{
+				Enabled:    true,
+				DecayStart: time.UnixMilli(1000),
+				DecayEnd:   time.UnixMilli(10000),
+			},
+		},
+		{
+			name: "should prevent validate decay information with enabled and start greater than end",
+			decayInfo: types.DecayInformation{
+				Enabled:    true,
+				DecayStart: time.UnixMilli(1001),
+				DecayEnd:   time.UnixMilli(1000),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.decayInfo.Validate()
+
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
