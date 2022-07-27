015ec1bc5f4a95ff70a3710e22982732f05e743c
ltacker <lucas.bertrand.22@gmail.com>
2022-07-27 12:09:14 +0200

    decay validate

---
+++ x/claim/types/decay.go
@@ -0,0 +1,12 @@
+package types
+
+import (
+	"fmt"
+)
+
+// Validate validates the decay information
+func (m DecayInformation) Validate() error {
+	if m.DecayStart.After(m.DecayEnd) {
+		return fmt.Errorf("decay starts after decay end %s > %s", m.DecayStart.String(), m.DecayEnd.String())
+	}
+}
