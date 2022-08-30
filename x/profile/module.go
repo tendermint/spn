3d0bfc789f48aaa62c582920aed9b38f891b9307
aljo242 <alex@shmeeload.xyz>
2022-08-30 12:15:12 -0400

    lint fix

--- x/profile/module.go
+++ x/profile/module.go
@@ -80,7 +80,7 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the profile module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx)) // nolint
+	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx)) //nolint
 }
 
 // GetTxCmd returns the profile module's root tx command.
